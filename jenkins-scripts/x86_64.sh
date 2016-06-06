316d6d888132c58c34a880d76ec33f3ac3d488bc
Trevor Bramwell <tbramwell@linuxfoundation.org>
2016-06-06 10:39:52 -0700

    Fix Docker Opts until the VM can be rebuilt
    Change-Id: I83ccb76f8ceea79fbe86ac0adf248455ec0c218e
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

---
+++ jenkins-scripts/x86_64.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Output the correct 'DOCKER_OPTS' to '/etc/default/docker'
+service docker stop
+echo "DOCKER_OPTS=\"-H tcp://0.0.0.0:2735 -H unix:///var/run/docker.sock\"" > /etc/default/docker
+service docker start
