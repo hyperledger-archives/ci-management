10bafc9c6a6db37676b77eaeb6edf9dbb9d18513
Trevor Bramwell <tbramwell@linuxfoundation.org>
2016-06-06 14:54:14 -0700

    Typoed the Docker Port
    Change-Id: Ic4ce3b8ac97abd0926032c493c35d215649decf5
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- jenkins-scripts/x86_64.sh
+++ jenkins-scripts/x86_64.sh
@@ -2,5 +2,5 @@
 
 # Output the correct 'DOCKER_OPTS' to '/etc/default/docker'
 service docker stop
-echo "DOCKER_OPTS=\"-H tcp://0.0.0.0:2735 -H unix:///var/run/docker.sock\"" > /etc/default/docker
+echo "DOCKER_OPTS=\"-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock\"" > /etc/default/docker
 service docker start
