02cbedd090061fc1c87ca464115e3a50a2807d1d
Trevor Bramwell <tbramwell@linuxfoundation.org>
2016-06-06 17:48:19 -0700

    Remove Docker Configuration from Jenkins Script
    These changes have been incorporated into the image.
    
    Change-Id: I80bc3ddc1a168138dded2bc356fb452e79e91d16
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- jenkins-scripts/x86_64.sh
+++ jenkins-scripts/x86_64.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-
-# Output the correct 'DOCKER_OPTS' to '/etc/default/docker'
-service docker stop
-echo "DOCKER_OPTS=\"-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock\"" > /etc/default/docker
-service docker start
+# x86_64 Specific Changes
