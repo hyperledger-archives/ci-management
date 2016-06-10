2f91703c792ddaa2238b0aef389e5f918c6754c6
Trevor Bramwell <tbramwell@linuxfoundation.org>
2016-06-09 17:19:55 -0700

    Move Fabic Clean Environment Job to own Script
    Change-Id: I7e0bfb2ece298bb21e62ea558c1165bd36dffd2d
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

---
+++ jjb/fabric/include-raw-fabric-clean-environment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -eu
+
+cd gopath/src/github.com/hyperledger/fabric
+
+docker stop hyperledger/fabric-{baseimage,src,ccenv,peer,membersrvc} || true
+make dist-clean || true
+docker rmi -f $(docker images | grep dev | awk '{print $3}') || true
