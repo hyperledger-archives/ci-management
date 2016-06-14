03944509e271d70ead1cf9fb4c9328f99aad9e43
Trevor Bramwell <tbramwell@linuxfoundation.org>
2016-06-13 19:35:28 -0700

    Rework Image Tags for Docker Build Job
    Change-Id: I64e35b8db17eca8aabe0d349323af13c34238928
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- jjb/fabric/include-raw-docker-image-deploy.sh
+++ jjb/fabric/include-raw-docker-image-deploy.sh
@@ -2,28 +2,24 @@
 set -o pipefail
 
 TAG=${GIT_COMMIT:0:7}
+PEER_IMAGE="$(docker images -q hyperledger/fabric-peer)"
+MEMBER_IMAGE="$(docker images -q hyperledger/fabric-membersrvc)"
 
 echo "COMMIT NUMBER: " $TAG
 echo "ARCH: " $ARCH
+echo "peer image: " $PEER_IMAGE
+echo "membersrvc image: " $MEMBER_IMAGE
 
-PEER_IMAGE="$(docker images -q hyperledger/fabric-peer)"
-MEMBER_IMAGE="$(docker images -q hyperledger/fabric-membersrvc)"
-PEER_TAG=$PEER_IMAGE:$ARCH-$TAG
-MEMBER_TAG=$MEMBER_IMAGE:$ARCH-$TAG
-
-echo "peer tag: " $PEER_TAG
-echo "membersrvc tag: " $MEMBER_TAG
-
-docker tag $PEER_TAG hyperledgergithub/fabric-peer:$TAG
-docker tag -f $PEER_TAG hyperledgergithub/fabric-peer:latest
-docker tag $MEMBER_TAG hyperledgergithub/fabric-membersrvc:$TAG
-docker tag -f $MEMBER_TAG hyperledgergithub/fabric-membersrvc:latest
+docker tag $PEER_IMAGE hyperledgergithub/fabric-peer:$ARCH-$TAG
+docker tag -f $PEER_IMAGE hyperledgergithub/fabric-peer:latest
+docker tag $MEMBER_IMAGE hyperledgergithub/fabric-membersrvc:$ARCH-$TAG
+docker tag -f $MEMBER_IMAGE hyperledgergithub/fabric-membersrvc:latest
 
 echo "--> Logging into Docker Hub"
 docker login --email=$DOCKER_HUB_EMAIL --username=$DOCKER_HUB_USERNAME --password=$DOCKER_HUB_PASSWORD
 
 echo "--> Pushing Docker Tags to Docker Hub"
-docker push hyperledgergithub/fabric-peer:$TAG
+docker push hyperledgergithub/fabric-peer:$ARCH-$TAG
 docker push hyperledgergithub/fabric-peer:latest
-docker push hyperledgergithub/fabric-membersrvc:$TAG
+docker push hyperledgergithub/fabric-membersrvc:$ARCH-$TAG
 docker push hyperledgergithub/fabric-membersrvc:latest
