9b329e2eec73e39d7248012b4d05ea140f7efb18
rameshthoomu <rameshbabu.thoomu@gmail.com>
2016-06-23 19:26:43 +0000

    tagging branch name to docker hub images
    Change-Id: I3aa7e327a5b61143182b8171f1db14e062957eac
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric/include-raw-docker-image-deploy.sh
+++ jjb/fabric/include-raw-docker-image-deploy.sh
@@ -7,19 +7,20 @@ MEMBER_IMAGE="$(docker images -q hyperledger/fabric-membersrvc)"
 
 echo "COMMIT NUMBER: " $TAG
 echo "ARCH: " $ARCH
+echo "Branch Name: " $branch
 echo "peer image: " $PEER_IMAGE
 echo "membersrvc image: " $MEMBER_IMAGE
 
-docker tag $PEER_IMAGE hyperledger/fabric-peer:$ARCH-$TAG
+docker tag $PEER_IMAGE hyperledger/fabric-peer:$ARCH-$branch-$TAG
 docker tag -f $PEER_IMAGE hyperledger/fabric-peer:latest
-docker tag $MEMBER_IMAGE hyperledger/fabric-membersrvc:$ARCH-$TAG
+docker tag $MEMBER_IMAGE hyperledger/fabric-membersrvc:$ARCH-$branch-$TAG
 docker tag -f $MEMBER_IMAGE hyperledger/fabric-membersrvc:latest
 
 echo "--> Logging into Docker Hub"
 docker login --email=$DOCKER_HUB_EMAIL --username=$DOCKER_HUB_USERNAME --password=$DOCKER_HUB_PASSWORD
 
 echo "--> Pushing Docker Tags to Docker Hub"
-docker push hyperledger/fabric-peer:$ARCH-$TAG
+docker push hyperledger/fabric-peer:$ARCH-$branch-$TAG
 docker push hyperledger/fabric-peer:latest
-docker push hyperledger/fabric-membersrvc:$ARCH-$TAG
+docker push hyperledger/fabric-membersrvc:$ARCH-$branch-$TAG
 docker push hyperledger/fabric-membersrvc:latest
