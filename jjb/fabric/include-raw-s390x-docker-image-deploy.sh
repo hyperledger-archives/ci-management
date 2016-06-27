532b692dfbfc7a9cb9eaa98d8232f6bdc78c77b7
rameshthoomu <rameshbabu.thoomu@gmail.com>
2016-06-27 18:19:24 +0000

    Updated docker hub repository names
    Change-Id: I71a9803b6e382ec4c8e1c38092dc1f29afdc757c
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

---
+++ jjb/fabric/include-raw-s390x-docker-image-deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -eu
+set -o pipefail
+
+BRANCH=${GIT_BRANCH##*/}
+TAG=${GIT_COMMIT:0:7}
+PEER_IMAGE="$(docker images -q hyperledger/fabric-peer)"
+MEMBERSERVC_IMAGE="$(docker images -q hyperledger/fabric-membersrvc)"
+
+echo "COMMIT NUMBER: " $TAG
+echo "Branch Name: " $BRANCH
+echo "peer image: " $PEER_IMAGE
+echo "membersrvc image: " $MEMBER_IMAGE
+
+docker tag $PEER_IMAGE hyperledger/fabric-s390x-peer:$BRANCH-$TAG
+docker tag -f $PEER_IMAGE hyperledger/fabric-s390x-peer:latest
+docker tag $MEMBERSERVC_IMAGE hyperledger/fabric-s390x-membersrvc:$BRANCH-$TAG
+docker tag -f $MEMBERSERVC_IMAGE hyperledger/fabric-s390x-membersrvc:latest
+
+echo "--> Logging into Docker Hub"
+docker login --email=$DOCKER_HUB_EMAIL --username=$DOCKER_HUB_USERNAME --password=$DOCKER_HUB_PASSWORD
+
+echo "--> Pushing Docker Tags to Docker Hub"
+docker push hyperledger/fabric-s390x-peer:$BRANCH-$TAG
+docker push hyperledger/fabric-s390x-peer:latest
+docker push hyperledger/fabric-s390x-membersrvc:$BRANCH-$TAG
+docker push hyperledger/fabric-s390x-membersrvc:latest
