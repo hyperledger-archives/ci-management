8e110626e039b6324f6ca3753e7a817e48f4f04d
rameshthoomu <rameshbabu.thoomu@gmail.com>
2016-06-29 17:42:50 +0000

    printing docker environment details added docker images and docker ps commands in output and cleanenvironment functions to troubleshoot behave failures and docker enviroment in x86 and Z machines Change-Id: I139290a1155b65811215ebc73b12a6aaea914e84 Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric/include-raw-fabric-clean-environment.sh
+++ jjb/fabric/include-raw-fabric-clean-environment.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -eu
 
 cd gopath/src/github.com/hyperledger/fabric
-
+docker images
+docker ps -a
 docker stop hyperledger/fabric-{baseimage,src,ccenv,peer,membersrvc} || true
 make dist-clean || true
 docker rmi -f $(docker images | grep dev | awk '{print $3}') || true
