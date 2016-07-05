06c3477c13e6fb94319cd9e78eb143fb1d753341
rameshthoomu <rameshbabu.thoomu@gmail.com>
2016-07-05 20:24:33 +0000

    removing docker stop command on docker images
    Change-Id: I8382dc1f242b2aa3921043a7efa47b03e4cb1a6f
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric/include-raw-fabric-clean-environment.sh
+++ jjb/fabric/include-raw-fabric-clean-environment.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -eu
-
 cd gopath/src/github.com/hyperledger/fabric
-docker images
-docker ps -a
-docker stop hyperledger/fabric-{baseimage,src,ccenv,peer,membersrvc} || true
 make dist-clean || true
 docker rmi -f $(docker images | grep dev | awk '{print $3}') || true
 docker rmi -f $(docker images | grep none | awk '{print $3}') || true
