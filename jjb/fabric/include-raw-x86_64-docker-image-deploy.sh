0bf7fa5098137dd51963be83e0a2e694ed5bf9fb
rameshthoomu <rameshbabu.thoomu@gmail.com>
2016-07-15 00:10:00 +0000

    updated docker hub repository name for x86_64 docker images
    Change-Id: I37949c35f18991de8e428daa2d4901b14b0ee0dd
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric/include-raw-x86_64-docker-image-deploy.sh
+++ jjb/fabric/include-raw-x86_64-docker-image-deploy.sh
@@ -11,16 +11,16 @@ echo "Branch Name: " $BRANCH
 echo "peer image: " $PEER_IMAGE
 echo "membersrvc image: " $MEMBERSERVC_IMAGE
 
-docker tag $PEER_IMAGE hyperledger/fabric-x86_64-peer:$BRANCH-$TAG
-docker tag -f $PEER_IMAGE hyperledger/fabric-x86_64-peer:latest
-docker tag $MEMBERSERVC_IMAGE hyperledger/fabric-x86_64-membersrvc:$BRANCH-$TAG
-docker tag -f $MEMBERSERVC_IMAGE hyperledger/fabric-x86_64-membersrvc:latest
+docker tag $PEER_IMAGE hyperledger/fabric-peer:$BRANCH-$TAG
+docker tag -f $PEER_IMAGE hyperledger/fabric-peer:latest
+docker tag $MEMBERSERVC_IMAGE hyperledger/fabric-membersrvc:$BRANCH-$TAG
+docker tag -f $MEMBERSERVC_IMAGE hyperledger/fabric-membersrvc:latest
 
 echo "--> Logging into Docker Hub"
 docker login --email=$DOCKER_HUB_EMAIL --username=$DOCKER_HUB_USERNAME --password=$DOCKER_HUB_PASSWORD
 
 echo "--> Pushing Docker Tags to Docker Hub"
-docker push hyperledger/fabric-x86_64-peer:$BRANCH-$TAG
-docker push hyperledger/fabric-x86_64-peer:latest
-docker push hyperledger/fabric-x86_64-membersrvc:$BRANCH-$TAG
-docker push hyperledger/fabric-x86_64-membersrvc:latest
+docker push hyperledger/fabric-peer:$BRANCH-$TAG
+docker push hyperledger/fabric-peer:latest
+docker push hyperledger/fabric-membersrvc:$BRANCH-$TAG
+docker push hyperledger/fabric-membersrvc:latest
