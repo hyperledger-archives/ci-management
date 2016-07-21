21755ea72e45bf112c3e93ac8bb26efe496d6189
Trevor Bramwell <tbramwell@linuxfoundation.org>
2016-07-21 15:35:29 -0700

    Install npm and fabric-node-sdk dependencies
    Change-Id: I8e5164ab991104a616f91f4b266cc827c9cc89bf
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- jenkins-scripts/x86_64.sh
+++ jenkins-scripts/x86_64.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 # x86_64 Specific Changes
+
+# install npm and fabric node sdk dependencies
+curl -sL https://deb.nodesource.com/setup_4.x | sudo bash -
+apt-get install -y nodejs
+
+npm install -g typescript
+npm install -g typings
