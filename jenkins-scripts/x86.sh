10d96c5be059d3f7b75cee10a43f644efe2886b3
Gabor Hosszu <gabor@digitalasset.com>
2016-07-29 17:11:12 -0400

    Install Maven3 on CI machines
    Change-Id: I32ffe126cf8689d8b6868040519e737ded8ff722
    Signed-off-by: Gabor Hosszu <gabor@digitalasset.com>

--- jenkins-scripts/x86.sh
+++ jenkins-scripts/x86.sh
@@ -4,6 +4,7 @@
 # install npm and fabric node sdk dependencies
 curl -sL https://deb.nodesource.com/setup_4.x | sudo bash -
 apt-get install -y nodejs
+apt-get install -y maven
 
 npm install -g typescript
 npm install -g typings
