e2df78cb3f912317b8a9a0dd8bad6e0a45ea2a7c
Ry Jones <rjones@linuxfoundation.org>
2016-08-24 12:08:43 -0700

    Move sed to init
    sed is causing build to be run twice.
    
    Change-Id: I7136f8d330aed501404b804c2e9b50b226170eb8
    Signed-off-by: Ry Jones <rjones@linuxfoundation.org>

--- jjb/fabric/include-raw-fabric-behave.sh
+++ jjb/fabric/include-raw-fabric-behave.sh
@@ -3,10 +3,6 @@ set -o pipefail
 
 cd gopath/src/github.com/hyperledger/fabric
 
-# Configure ip/port
-ip="$(ifconfig docker0 | grep 'inet[^6]' | awk '{print $2}' | cut -d ':' -f 2)"
-port=2375
 # script
 echo "Executing Behave test scripts"
-sed -i -e 's/172.17.0.1:2375\b/'"$ip:$port"'/g' bddtests/compose-defaults.yml
 make behave BEHAVE_OPTS="-D logs=Y -o testsummary.log --junit --format=pretty"
