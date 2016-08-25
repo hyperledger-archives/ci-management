c4b1da25e463a06485ce6b6ff2210fd9baa26fdc
rameshthoomu <rameshbabu.thoomu@gmail.com>
2016-08-25 18:51:27 -0400

    Fix docker push issue in Jenkins CI
    docker push is failed in Jenkins CI after merge
    https://gerrit.hyperledger.org/r/#/c/571/
    
    Change-Id: Ie6d54ae149b846847ef1355705a084116fdef13f
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric/include-raw-x86_64-docker-image-deploy.sh
+++ jjb/fabric/include-raw-x86_64-docker-image-deploy.sh
@@ -1,26 +1,20 @@
 #!/bin/bash -eu
 set -o pipefail
 
-BRANCH=${GIT_BRANCH##*/}
-TAG=${GIT_COMMIT:0:7}
-PEER_IMAGE="$(docker images -q hyperledger/fabric-peer)"
-MEMBERSERVC_IMAGE="$(docker images -q hyperledger/fabric-membersrvc)"
+TAG="$(docker images hyperledger/fabric-peer | awk 'NR!=1 &&  $2!="latest" {print $2}')"
+PEER_IMAGE="$(docker images hyperledger/fabric-peer | awk 'NR!=1 &&  $2!="latest" {print $3}')"
+MEMBERSERVC_IMAGE="$(docker images hyperledger/fabric-membersrvc | awk 'NR!=1 &&  $2!="latest" {print $3}')"
 
-echo "COMMIT NUMBER: " $TAG
-echo "Branch Name: " $BRANCH
+echo "PEER TAG: " $TAG
+echo "MEMBERSRVC TAG: " $TAG
 echo "peer image: " $PEER_IMAGE
 echo "membersrvc image: " $MEMBERSERVC_IMAGE
-
-docker tag $PEER_IMAGE hyperledger/fabric-peer:$BRANCH-$TAG
-docker tag -f $PEER_IMAGE hyperledger/fabric-peer:latest
-docker tag $MEMBERSERVC_IMAGE hyperledger/fabric-membersrvc:$BRANCH-$TAG
-docker tag -f $MEMBERSERVC_IMAGE hyperledger/fabric-membersrvc:latest
 
 echo "--> Logging into Docker Hub"
 docker login --email=$DOCKER_HUB_EMAIL --username=$DOCKER_HUB_USERNAME --password=$DOCKER_HUB_PASSWORD
 
 echo "--> Pushing Docker Tags to Docker Hub"
-docker push hyperledger/fabric-peer:$BRANCH-$TAG
+docker push hyperledger/fabric-peer:$TAG
 docker push hyperledger/fabric-peer:latest
-docker push hyperledger/fabric-membersrvc:$BRANCH-$TAG
+docker push hyperledger/fabric-membersrvc:$TAG
 docker push hyperledger/fabric-membersrvc:latest
