c325fb93e5b2a0451fea2b66398267616c877c90
rameshthoomu <rameshbabu.thoomu@gmail.com>
2016-09-08 15:56:26 +0000

    disable fabric behave tests temporarily in CI
    Chris mentioned in slack channel to disable fabric behave tests
    temporarily until we stabilize them. Disabled these tests in x86 and z
    build jobs
    
    Change-Id: I7847d5df0c95b2c4d9a70e81d3cb9d9eabfb58ff
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric/include-raw-fabric-clean-environment.sh
+++ jjb/fabric/include-raw-fabric-clean-environment.sh
@@ -4,7 +4,7 @@ cd gopath/src/github.com/hyperledger/fabric
 ip="$(ifconfig docker0 | grep 'inet[^6]' | awk '{print $2}' | cut -d ':' -f 2)"
 port=2375
 # script
-sed -i -e 's/172.17.0.1:2375\b/'"$ip:$port"'/g' bddtests/compose-defaults.yml
+#sed -i -e 's/172.17.0.1:2375\b/'"$ip:$port"'/g' bddtests/compose-defaults.yml
 make dist-clean || true
 docker rmi -f $(docker images | grep dev | awk '{print $3}') || true
 docker rmi -f $(docker images | grep none | awk '{print $3}') || true
