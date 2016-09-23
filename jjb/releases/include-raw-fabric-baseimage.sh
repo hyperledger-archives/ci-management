7c9bd98dc0c2e344f2a8dcc117caa2197cd54007
ramesthoomu <rameshbabu.thoomu@gmail.com>
2016-09-23 15:30:48 +0000

    FAB-436 publish docker and vagrant baseimages
    This patch creates verify, merge and release jobs to verify fabric-baseimage
    changes. After merge jobs are successfully executed, maintainers has
    to manually trigger the release job (fabric-baseimage-x86_64)
    which automatically triggers all release jobs and push images to docker hub.
    
    Change-Id: Id9c1433c7af610a428c10823ace2ba3d34b639bc
    Signed-off-by: ramesthoomu <rameshbabu.thoomu@gmail.com>

---
+++ jjb/releases/include-raw-fabric-baseimage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -o pipefail
+
+IS_RELEASE=`cat Makefile | grep IS_RELEASE | awk '{print $3 }'`
+if [ "$IS_RELEASE" == "true" ];
+then
+       echo "Release detected!  Triggering release promotion"
+    exit 0
+fi
+
+echo "SNAPSHOT release detected, skipping release promotion"
+exit -1
