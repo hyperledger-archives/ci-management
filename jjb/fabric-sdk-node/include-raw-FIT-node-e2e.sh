ea3f54f4cddbfa70c4135ffa830987260dfcf955
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-01-19 03:49:52 +0000

    remove fabric-cop references in FIT job
    Change-Id: I08bf208e0fafae08a0ed924839a41eba4ec9720c
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric-sdk-node/include-raw-FIT-node-e2e.sh
+++ jjb/fabric-sdk-node/include-raw-FIT-node-e2e.sh
@@ -14,16 +14,16 @@ FABRIC_COMMIT=$(git log -1 --pretty=format:"%h")
 make docker
 docker images | grep hyperledger
 
-# Clone fabric-cop git repository
+# Clone fabric-ca git repository
 ################################
 
-rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-cop
+rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-ca
 
-WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-cop"
-COP_REPO_NAME=fabric-cop
-git clone --depth=1 https://github.com/hyperledger/$COP_REPO_NAME.git $WD
+WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-ca"
+CA_REPO_NAME=fabric-ca
+git clone --depth=1 https://github.com/hyperledger/$CA_REPO_NAME.git $WD
 cd $WD
-COP_COMMIT=$(git log -1 --pretty=format:"%h")
+CA_COMMIT=$(git log -1 --pretty=format:"%h")
 make docker
 docker images | grep hyperledger
 
@@ -36,7 +36,10 @@ SDK_REPO_NAME=fabric-sdk-node
 git clone --depth=1 https://github.com/hyperledger/$SDK_REPO_NAME.git $WD
 cd $WD
 SDK_COMMIT=$(git log -1 --pretty=format:"%h")
+
+echo "=======>" "FABRIC PEER COMMIT NUMBER" "-" $FABRIC_COMMIT "=======>" "FABRIC CA COMMIT NUMBER" "-" $CA_COMMIT "=======>" "FABRIC SDK NODE COMMIT NUMBER" "-" $SDK_COMMIT >> commit_history.log
 cd test/fixtures
+cat docker-compose.yml > docker-compose.log
 docker-compose up >> dockerlogfile.log 2>&1 &
 docker ps
 cd ../.. && npm install
@@ -45,4 +48,3 @@ gulp && gulp cop
 rm -rf node_modules/hfc-cop && npm install
 istanbul cover --report cobertura test/unit/end-to-end.js
 
-echo "=======>" "FABRIC COMMIT NUMBER" "-" $FABRIC_COMMIT "=======>" "FABRIC COP COMMIT NUMBER" "-" $COP_COMMIT "=======>" "FABRIC SDK NODE COMMIT NUMBER" "-" $SDK_COMMIT >> commit_history.log
