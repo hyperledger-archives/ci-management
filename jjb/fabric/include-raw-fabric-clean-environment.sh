45b6dd41f4b2fa1359810ad802dad53e38c18d1e
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-02-21 16:31:31 +0000

    cleanup docker images before bootstrap dev environment
    make clean from fabric-ca is not removing docker-images with snapshot
    tags. More number of images are stuck in power and z builds and it's not clearing
    Also updated script to remove containers first and then delete all the images except
    baseimage and baseos.
    
    Change-Id: Ide9c0bbfc824932811e40518e649be3d6c28a9d8
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric/include-raw-fabric-clean-environment.sh
+++ jjb/fabric/include-raw-fabric-clean-environment.sh
@@ -1,11 +1,8 @@
 #!/bin/bash -eu
+
 cd gopath/src/github.com/hyperledger/fabric
-# Configure ip/port
-ip="$(ifconfig docker0 | grep 'inet[^6]' | awk '{print $2}' | cut -d ':' -f 2)"
-port=2375
-# script
-sed -i -e 's/172.17.0.1:2375\b/'"$ip:$port"'/g' bddtests/compose-defaults.yml
-make dist-clean || true
-docker rmi -f $(docker images | grep dev | awk '{print $3}') || true
-docker rmi -f $(docker images | grep none | awk '{print $3}') || true
+
 docker rm -f $(docker ps -aq) || true
+make dist-clean || true
+docker rmi -f $(docker images | grep -v 'hyperledger/fabric-base*' | awk {'print $3'}) || true
+
