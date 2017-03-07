9692883c15bf24f8daa1c586d86b7625f4258369
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-03-07 16:35:06 +0000

    pull commits based on condition
    checkout to a specific commit level if the commit level
    is not latest otherwise continue with the latest commit
    levels.
    
    Change-Id: I926e0e881fb48f71de38872e832d56d819f1b750
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric-sdk-java/include-raw-fabric-sdk-java-end-to-end-tests.sh
+++ jjb/fabric-sdk-java/include-raw-fabric-sdk-java-end-to-end-tests.sh
@@ -12,8 +12,14 @@ WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric"
 REPO_NAME=fabric
 git clone https://github.com/hyperledger/$REPO_NAME.git $WD
 cd $WD
+if [ "$FABRIC_COMMIT" == "latest" ]; then
+echo "Fabric commit is $FABRIC_COMMIT so go with this"
+else
 git checkout $FABRIC_COMMIT
 FABRIC_COMMIT_LEVEL=$(git log -1 --pretty=format:"%h")
+fi
+FABRIC_COMMIT_LEVEL=$(git log -1 --pretty=format:"%h")
+# Build fabric Docker images
 make docker
 docker images | grep hyperledger
 
@@ -26,12 +32,18 @@ WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-ca"
 CA_REPO_NAME=fabric-ca
 git clone https://github.com/hyperledger/$CA_REPO_NAME.git $WD
 cd $WD
+if [ "$FABRIC_CA_COMMIT" == "latest" ]; then
+echo "Fabric_CA commit is $FABRIC_COMMIT so go with this"
+else
 git checkout $FABRIC_CA_COMMIT
 CA_COMMIT_LEVEL=$(git log -1 --pretty=format:"%h")
+fi
+CA_COMMIT_LEVEL=$(git log -1 --pretty=format:"%h")
+# Build CA Docker Images
 make docker
 docker images | grep hyperledger
 
-# Move to fabric-sdk-java repository and execute end-to-end tests
+# Move to fabric-sdk-java repository and execute SDK end-to-end tests
 
 export WD=${WORKSPACE}
 cd $WD
