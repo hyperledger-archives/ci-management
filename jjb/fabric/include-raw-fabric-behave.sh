1dc9aa8612859cce14682dc729541d5e950e0756
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-03-07 22:32:45 +0000

    FAB-2666: behave Job for fabric verify & merge commits
    Enable behave CI job to verify behave bootstrap tests on every fabric
    verify and merge commits. We enabled this on x86_64 for now, but will
    enable this on all other platforms later once it is stable.
    
    Change-Id: I331255009973a08884e892ded4ba10c3b96878bf
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric/include-raw-fabric-behave.sh
+++ jjb/fabric/include-raw-fabric-behave.sh
@@ -1,8 +1,32 @@
 #!/bin/bash -eu
 set -o pipefail
 
-cd gopath/src/github.com/hyperledger/fabric
+cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric
 
-# script
-echo "Executing Behave test scripts"
-make behave BEHAVE_OPTS="-D logs=Y -o testsummary.log --junit --format=pretty"
+# Create virtual Environment to test behave tests
+
+virtualenv -p /usr/bin/python2.7 venv
+export PS1="test"
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7
+source venv/bin/activate
+
+# Install all required python modules
+
+pip install behave
+pip install grpcio-tools
+pip install "pysha3==1.0b1"
+pip install b3j0f.aop
+pip install jinja2
+pip install pyopenssl
+pip install ecdsa
+pip install python-slugify
+pip install pyyaml
+
+
+# Build Peer and images
+
+make peer && make peer-docker && make orderer-docker
+docker images | grep hyperledger
+cd bddtests
+behave -k -D cache-deployment-spec
+#behave -k -D cache-deployment-spec features/bootstrap.feature
