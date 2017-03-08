dab65a734e344df9a1709d9f3a6400523bdb960d
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-03-08 05:05:37 +0000

    FAB-2666 enable CLI tests in CI
    This patch includes end-to-end tests (cli), (node) and (java) runs
    for every fabric verify and merge commits. We intially run only on
    x86_64 arch. Will add job to run these tests on Power and z.
    
    skipping voting on failure to gerrit. Once the tests are stable, will verify
    gerrit voting for verify and merge.
    
    Change-Id: Ie9198d0162914e5a205968f66c85f2da4a01adc6
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

---
+++ jjb/fabric/include-raw-FIT-NODE-e2e.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -exu
+set -o pipefail
+
+# RUN END-to-END Test
+#####################
+rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-sdk-node
+
+WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-sdk-node"
+SDK_REPO_NAME=fabric-sdk-node
+git clone https://github.com/hyperledger/$SDK_REPO_NAME.git $WD
+cd $WD
+SDK_NODE_COMMIT=$(git log -1 --pretty=format:"%h")
+echo "SDK_NODE_COMMIT=======> $SDK_NODE_COMMIT" >> ${WORKSPACE}/gopath/src/github.com/hyperledger/commit.log
+cd test/fixtures
+docker rm -f $(docker ps -aq) || true
+docker-compose up >> node_dockerlogfile.log 2>&1 &
+sleep 10
+docker ps -a
+cd ../.. && npm install
+npm config set prefix ~/npm && npm install -g gulp && npm install -g istanbul
+gulp && gulp ca
+rm -rf node_modules/fabric-ca-client && npm install
+istanbul cover --report cobertura test/integration/end-to-end.js
+docker rm -f $(docker ps -aq) || true
