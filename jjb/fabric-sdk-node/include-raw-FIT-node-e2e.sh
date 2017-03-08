1e3e06d1b8488882bfe79ab4a3a412cb1a5302c8
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-03-08 21:22:03 +0000

    update FIT job with node e2e test instructions
    Change-Id: Ice093ac4a650dc24ae7e64f511e087dc6813581d
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric-sdk-node/include-raw-FIT-node-e2e.sh
+++ jjb/fabric-sdk-node/include-raw-FIT-node-e2e.sh
@@ -8,7 +8,7 @@ rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric
 
 WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric"
 REPO_NAME=fabric
-git clone --depth=1 https://github.com/hyperledger/$REPO_NAME.git $WD
+git clone https://github.com/hyperledger/$REPO_NAME.git $WD
 cd $WD
 FABRIC_COMMIT=$(git log -1 --pretty=format:"%h")
 make docker
@@ -21,7 +21,7 @@ rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-ca
 
 WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-ca"
 CA_REPO_NAME=fabric-ca
-git clone --depth=1 https://github.com/hyperledger/$CA_REPO_NAME.git $WD
+git clone https://github.com/hyperledger/$CA_REPO_NAME.git $WD
 cd $WD
 CA_COMMIT=$(git log -1 --pretty=format:"%h")
 make docker
@@ -33,19 +33,19 @@ rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-sdk-node
 
 WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-sdk-node"
 SDK_REPO_NAME=fabric-sdk-node
-git clone --depth=1 https://github.com/hyperledger/$SDK_REPO_NAME.git $WD
+git clone https://github.com/hyperledger/$SDK_REPO_NAME.git $WD
 cd $WD
 SDK_COMMIT=$(git log -1 --pretty=format:"%h")
 
 echo "=======>" "FABRIC PEER COMMIT NUMBER" "-" $FABRIC_COMMIT "=======>" "FABRIC CA COMMIT NUMBER" "-" $CA_COMMIT "=======>" "FABRIC SDK NODE COMMIT NUMBER" "-" $SDK_COMMIT >> commit_history.log
-cd test/fixtures
-cat docker-compose.yml > docker-compose.log
+cd test/fixtures/channel
+cat docker-compose.yaml > docker-compose.log
 docker-compose up >> dockerlogfile.log 2>&1 &
 sleep 10
-docker ps
-cd ../.. && npm install
+docker ps -a
+cd ../../.. && npm install
 npm config set prefix ~/npm && npm install -g gulp && npm install -g istanbul
 gulp && gulp ca
 rm -rf node_modules/fabric-ca-client && npm install
-istanbul cover --report cobertura test/integration/end-to-end.js
+istanbul cover --report cobertura test/integration/e2e.js
 
