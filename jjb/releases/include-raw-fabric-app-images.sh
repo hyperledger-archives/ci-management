618d62a7fb914c39367c79bf8bc18f530e230c6e
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-03-13 18:23:47 +0000

    update scripts to publish dockerimages to dockerhub
    This patch includes build fabric and fabric-ca docker images
    and publish them to hyperledger dockerhub account after tag
    is created in fabric repository.
    
    Release jobs triggers only after tag is created in fabric repo.
    Please note, verify and merge jobs are disabled.
    
    Change-Id: I9df827a143b6257446eb4768b0ccc922c474cf97
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

---
+++ jjb/releases/include-raw-fabric-app-images.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -o pipefail
+
+FABRIC_TAG=$(docker inspect --format "{{ .RepoTags }}" hyperledger/fabric-peer | sed 's/.*:\(.*\)]/\1/')
+echo "FABRIC Images TAG ID is: " $FABRIC_TAG
+echo
+ORG_NAME="hyperledger/fabric"
+
+# Push docker images to nexus repository
+
+dockerFabricPush() {
+
+  for IMAGES in peer orderer couchdb ccenv javaenv kafka zookeeper; do
+    echo "==> $IMAGES"
+    docker push $ORG_NAME-$IMAGES:$FABRIC_TAG
+    echo
+    echo "==> $ORG_NAME-$IMAGES:$FABRIC_TAG"
+    echo
+  done
+}
+
+# Push Fabric Docker Images to Nexus Repository
+dockerFabricPush
+
+# Listout all docker images Before and After Push to NEXUS
+docker images | grep hyperledger*
