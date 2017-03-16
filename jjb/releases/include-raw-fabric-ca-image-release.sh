c15be431599e09f82c7012d0107e6c518b5956b5
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-03-16 16:31:05 +0000

    Add Docker login command
    update docker login command to push docker images to dockerhub account
    
    Change-Id: I6d404fe433c6bdfabba09f4f85dc9590285ec9b9
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/releases/include-raw-fabric-ca-image-release.sh
+++ jjb/releases/include-raw-fabric-ca-image-release.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -o pipefail
 
+docker login --email=$DOCKER_HUB_EMAIL --username=$DOCKER_HUB_USERNAME --password=$DOCKER_HUB_PASSWORD
+
 TAG=$(docker inspect --format "{{ .RepoTags }}" hyperledger/fabric-ca | sed 's/.*:\(.*\)]/\1/')
 
 echo "Image TAG ID is: " $TAG
