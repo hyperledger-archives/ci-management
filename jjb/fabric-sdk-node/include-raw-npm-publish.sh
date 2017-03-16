b19a27bb20c6ec4acf4bd36e3e84f7822118252a
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-03-16 20:06:30 +0000

    FAB-2802 publish npm modules
    Publish npm modules after every fabric-sdk-node merge job.
    
    Publish npm version as unstable (npm publish --tags unstable) if the
    current version in package.json is -snapshot.
    
    Publish npm module as a Release if the current version in package.jon
    is -alpha
    
    https://jira.hyperledger.org/browse/FAB-2802
    
    Change-Id: I1b4fa94cff5b681f4195ab34660abee6eb39e07d
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric-sdk-node/include-raw-npm-publish.sh
+++ jjb/fabric-sdk-node/include-raw-npm-publish.sh
@@ -3,37 +3,64 @@
 #################################################
 #Publish npm module as unstable after merge commit
 #npm publish --tag unstable
-#Run this "npm view fabric-ca-client" then look for
-#"dist-tags"
+#Run this "npm dist-tags ls $pkgs then look for
+#unstable versions
 #################################################
 
 set -o pipefail
 
 npmPublish() {
+  if [ $RELEASE = "snapshot" ]; then
+    echo
+    UNSTABLE_VER=$(npm dist-tags ls "$1" | awk '/unstable/{
+    ver=$NF
+    sub(/.*\./,"",rel)
+    sub(/\.[[:digit:]]$/,"",ver)
+    print ver}')
 
-BASE_VERSION=$(echo $CURRENT_VER | cut -d"." -f1-2)
-VER_INCREMENT=$(echo $CURRENT_VER | cut -d"." -f3)
-INCREMENT=`expr $VER_INCREMENT + 1`
-sed -i 's/\(.*\"version\"\: \"'$BASE_VERSION'\)\(.*\)/\1.'$INCREMENT\"\,'/' package.json
-NEW_RELEASE=$(cat package.json | grep version | awk -F\" '{ print $4 }')
-echo "===> $NEW_RELEASE"
-echo
-echo "===> Publish $NEW_RELEASE as unstable"
-npm publish --tag unstable
+    echo "===> UNSTABLE VERSION --> $UNSTABLE_VER"
 
+    UNSTABLE_INCREMENT=$(npm dist-tags ls "$1" | awk '/unstable/{
+    ver=$NF
+    rel=$NF
+    sub(/.*\./,"",rel)
+    sub(/\.[[:digit:]]$/,"",ver)
+    print ver"."rel+1}')
+
+    echo "===> Incremented UNSTABLE VERSION --> $UNSTABLE_INCREMENT"
+
+    if [ "$UNSTABLE_VER" = "$CURRENT_RELEASE" ]; then
+      # Replace existing version with Incremented $UNSTABLE_VERSION
+      sed -i 's/\(.*\"version\"\: \"\)\(.*\)/\1'$UNSTABLE_INCREMENT\"\,'/' package.json
+      npm publish --tag unstable
+    else
+      # Replace existing version with $CURRENT_RELEASE
+      sed -i 's/\(.*\"version\"\: \"\)\(.*\)/\1'$CURRENT_RELEASE\"\,'/' package.json
+      npm publish --tag unstable
+    fi
+
+  elif [ $RELEASE = "alpha" ]; then
+    echo "===> PUBLISH --> $CURRENT_RELEASE"
+    npm publish
+  fi
 }
 
-cd $WORKSPACE/gopath/src/github.com/hyperledger/fabric-sdk-node
+versions() {
+
+  CURRENT_RELEASE=$(cat package.json | grep version | awk -F\" '{ print $4 }')
+  echo "===> Current Version --> $CURRENT_RELEASE"
+
+  RELEASE=$(cat package.json | grep version | awk -F\" '{ print $4 }' | cut -d "-" -f 2)
+  echo "===> Current Release --> $RELEASE"
+}
+
+cd $WORKSPACE/src/github.com/hyperledger/fabric-sdk-node
 npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN
 
 cd fabric-ca-client
-echo
-CURRENT_VER=$(npm show fabric-ca-client@* version |  tail -1 | awk '{print $1}' | cut -d '@' -f 2)
-echo "===> Current Fabric-ca-client version $CURRENT_VER"
-npmPublish
+versions
+npmPublish fabric-ca-client
 
 cd ../fabric-client
-echo
-CURRENT_VER=$(npm show fabric-client@* version |  tail -1 | awk '{print $1}' | cut -d '@' -f 2)
-echo "===> Current Fabric-client version $CURRENT_VER"
-npmPublish
+versions
+npmPublish fabric-client
