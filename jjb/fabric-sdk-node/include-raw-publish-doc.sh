24cf8fe8b97c3dcafb7679351e2719004c5c0ebd
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-03-24 00:35:19 +0000

    FAB-164 publish nodesdk API docs to gh-pages
    publish node sdk API docs to git gh-pages repository.
    Copy and push the docs after generate them using gulp doc.
    
    Change-Id: I3a304c0f06ac18d2e8310a00e2c5ec6e1868a8d9
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

---
+++ jjb/fabric-sdk-node/include-raw-publish-doc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -eu
+set -o pipefail
+
+cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-sdk-node
+# Generate sdk docs
+gulp doc
+SDK_COMMIT=$(git rev-parse --short HEAD)
+TARGET_REPO=$NODE_SDK_USERNAME.github.io.git
+git clone https://github.com/$NODE_SDK_USERNAME/$TARGET_REPO
+
+# Copy API docs to Target repository
+cp -r docs/gen/* $NODE_SDK_USERNAME.github.io
+cd $NODE_SDK_USERNAME.github.io
+git add .
+git commit -m "SDK commit - $SDK_COMMIT"
+git config remote.gh-pages.url https://$NODE_SDK_USERNAME:$NODE_SDK_PASSWORD@github.com/$NODE_SDK_USERNAME/$TARGET_REPO
+
+# Push API docs to Target repository
+git push gh-pages master
