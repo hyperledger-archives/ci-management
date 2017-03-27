a57bc43ac2c0e0457dc61932ad427859ae1d9f3e
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-03-27 03:57:59 +0000

    Revert "Emit contents of several files"
    This reverts commit 4e9d8ce48c84e7fb2c955dc5784dda6ee4a13e1c.
    
    Change-Id: Idba0e465340a3ea9d1802edcff4cb1602ae76366
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jenkins-scripts/create_jenkins_user.sh
+++ jenkins-scripts/create_jenkins_user.sh
@@ -11,33 +11,12 @@ OS=$(facter operatingsystem | tr '[:upper:]' '[:lower:]')
 useradd -m -G docker -s /bin/bash jenkins
 mkdir /home/jenkins/.ssh
 mkdir /w
-echo #######################
-echo # EMIT CONTENTS OF /home/$os/.ssh/authorized_keys
-echo #######################
-cat /home/$os/.ssh/authorized_keys
-echo #######################
-echo # EMITTED CONTENTS OF /home/$os/.ssh/authorized_keys
-echo #######################
 while [ `wc -l<"/home/${OS}/.ssh/authorized_keys"` -lt 2 ]
 do
   echo Not enough keys.
   sleep 5
 done
-echo #######################
-echo # EMIT CONTENTS OF /home/$os/.ssh/authorized_keys
-echo #######################
-cat /home/$os/.ssh/authorized_keys
-echo #######################
-echo # EMITTED CONTENTS OF /home/$os/.ssh/authorized_keys
-echo #######################
 cp -r "/home/${OS}/.ssh/authorized_keys" /home/jenkins/.ssh/authorized_keys
-echo #######################
-echo # EMIT CONTENTS OF /home/jenkins/.ssh/authorized_keys
-echo #######################
-cat /home/jenkins/.ssh/authorized_keys
-echo #######################
-echo # EMITTED CONTENTS OF /home/jenkins/.ssh/authorized_keys
-echo #######################
 # Generate ssh key for use by Robot jobs
 echo -e 'y\n' | ssh-keygen -N "" -f /home/jenkins/.ssh/id_rsa -t rsa
 chown -R jenkins:jenkins /home/jenkins/.ssh /w
