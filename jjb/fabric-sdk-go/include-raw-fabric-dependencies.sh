c78fe19121e9a8cac1b05c6de91f05efe9100ad0
Divyank Katira <Divyank.Katira@securekey.com>
2017-04-07 12:22:30 -0400

    [FAB-3027] Updated fabric-sdk-go scripts
    Added Jenkins  builder to run integration tests
    
    Change-Id: I53cd74ec52a6bcfe402d0a78cb16c627d386899a
    Signed-off-by: Divyank Katira <Divyank.Katira@securekey.com>

---
+++ jjb/fabric-sdk-go/include-raw-fabric-dependencies.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -exu
+set -o pipefail
+
+source ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-sdk-go/test/scripts/fabric_test_commitlevel.sh
+
+# Clone fabric git repository
+#############################
+
+rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric
+
+WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric"
+REPO_NAME=fabric
+git clone ssh://hyperledger-jobbuilder@gerrit.hyperledger.org:29418/$REPO_NAME $WD
+cd $WD
+if [ "$FABRIC_COMMIT" == "latest" ]; then
+echo "Fabric commit is $FABRIC_COMMIT so go with this"
+else
+git checkout $FABRIC_COMMIT
+FABRIC_COMMIT_LEVEL=$(git log -1 --pretty=format:"%h")
+fi
+FABRIC_COMMIT_LEVEL=$(git log -1 --pretty=format:"%h")
+# Build fabric Docker images
+make docker
+docker images | grep hyperledger
+
+# Clone fabric-ca git repository
+################################
+
+rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-ca
+
+WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-ca"
+CA_REPO_NAME=fabric-ca
+git clone ssh://hyperledger-jobbuilder@gerrit.hyperledger.org:29418/$CA_REPO_NAME $WD
+cd $WD
+if [ "$FABRIC_CA_COMMIT" == "latest" ]; then
+echo "Fabric_CA commit is $FABRIC_COMMIT so go with this"
+else
+git checkout $FABRIC_CA_COMMIT
+CA_COMMIT_LEVEL=$(git log -1 --pretty=format:"%h")
+fi
+CA_COMMIT_LEVEL=$(git log -1 --pretty=format:"%h")
+# Build CA Docker Images
+make docker
+docker images | grep hyperledger
+
+# Move to fabric-sdk-go repository and execute integration tests
+export WD=${WORKSPACE}
+cd $WD
+GO_SDK_COMMIT_LEVEL=$(git log -1 --pretty=format:"%h")
+echo "=======>" "FABRIC COMMIT NUMBER" "-" $FABRIC_COMMIT_LEVEL "=======>" "FABRIC CA COMMIT NUMBER" "-" $CA_COMMIT_LEVEL "=======>" "FABRIC SDK GO COMMIT NUMBER" "-" $GO_SDK_COMMIT_LEVEL >> commit_history.log
+export GOPATH=$WD/gopath
+
+cd $WD
+make integration-test
