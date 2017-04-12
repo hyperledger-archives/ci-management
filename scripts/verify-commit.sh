6bf35a0a61f3ef9341e5a010302f094fbc7eeb9e
Jessica Wagantall <jwagantall@linuxfoundation.org>
2017-04-12 12:21:03 -0700

    verify-commit.sh: Verify trailing white spaces
    We need to make sure that new commits won't add trailing
    white spaces in the code
    
    Change-Id: I6f892de6925574a0b0282cf8c3a68a44ca12850b
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>

--- scripts/verify-commit.sh
+++ scripts/verify-commit.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
+# This script makes several basic commit message validations.
+# This is with the purpose of keeping up with the aesthetics
+# of our code.
+
+# Verify if the commit message contains JIRA URLs.
+# its-jira pluggin attempts to process jira links and breaks.
+
 JIRA_LINK=`git rev-list --format=%B --max-count=1 HEAD | grep -io 'http[s]*://jira\..*'`
 if [[ ! -z "$JIRA_LINK" ]]
 then
-  echo 'Remove JIRA URLs from commit message'
+  echo 'Error: Remove JIRA URLs from commit message'
   echo 'Add jira references as: Issue: <JIRAKEY>-<ISSUE#>, instead of URLs'
+  exit 1
+fi
+
+# Verify if there are trailing spaces in the files.
+
+COMMIT_FILES=`git diff-tree --name-only -r HEAD~1..HEAD`
+
+for filename in `echo $COMMIT_FILES`; do
+  if [ ! -z `egrep -l " +$" $filename` ];
+  then
+    FOUND_TRAILING='yes'
+    echo "Error: Trailing white spaces found in file:$filename"
+  fi
+done
+
+if [ ! -z ${FOUND_TRAILING+x} ];
+then
   exit 1
 fi
