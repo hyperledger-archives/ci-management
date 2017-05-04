6d2966ec32218fa12b826fd2aac69d97e6570628
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-05-03 23:24:18 -0400

    FAB-3123 publish fabric binaries to nexus
    This patch includes script to publish fabric binaries cryptogen
    and configtxgen to nexus repository. Script does below tasks before
    push to nexus
    
     - copy chaincodes (example02 and marbles) to release/binary directory
     - copy artifacts in e2e_cli folder to release directory
     - create a tar.gz of release directory
     - push each binary tar file to nexus repository based on the
    Base Version check result
    
    Change-Id: I2890fc1aa1552feba46e17cc5e2f00f6cb666543
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

---
+++ jjb/fabric/include-raw-fabric-push-fabric-binaries.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -o pipefail
+
+export FABRIC_ROOT_DIR=$WORKSPACE/gopath/src/github.com/hyperledger/fabric
+
+cd $FABRIC_ROOT_DIR
+
+echo "=======> Build binaries for all platforms"
+make release-clean release-all
+
+BASE_VERSION=`cat Makefile | grep BASE_VERSION | awk '{print $3}' | head -1`
+echo "=============> $BASE_VERSION"
+
+COMMIT_VERSION=$(git rev-parse --short HEAD)
+echo "=============> $COMMIT_VERSION"
+
+IS_RELEASE=`cat Makefile | grep IS_RELEASE | awk '{print $3}'`
+echo "=======>" $IS_RELEASE
+
+if [ "${IS_RELEASE}" == "false" ]; then
+
+# copy e2e_cli folder to release/$binary
+     for binary in linux-amd64 windows-amd64 darwin-amd64 linux-ppc64le linux-s390x; do
+       TMP=$(echo "$binary" | tr -d '-')
+       mkdir -p release/$binary/chaincode/go/chaincode_example02 release/$binary/chaincode/go/marbles02
+       cp $FABRIC_ROOT_DIR/examples/chaincode/go/chaincode_example02/chaincode_example02.go release/$binary/chaincode/go/chaincode_example02/
+       cp $FABRIC_ROOT_DIR/examples/chaincode/go/marbles02/marbles_chaincode.go release/$binary/chaincode/go/marbles02/
+       cp -ar examples/e2e_cli/. release/$binary && rm -rf release/$binary/examples && sed -i "s/e2ecli/$TMP/g" release/$binary/base/peer-base.yaml && tar -czf fabric-e2e-$binary-$BASE_VERSION-snapshot-$COMMIT_VERSION.tar.gz release/$binary release/chaincode
+       echo "Pushing fabric-e2e-$binary-$BASE_VERSION-snapshot-$COMMIT_VERSION.tar.gz to maven snapshots..."
+       mvn org.apache.maven.plugins:maven-deploy-plugin:deploy-file \
+        -Dfile=$WORKSPACE/gopath/src/github.com/hyperledger/fabric/fabric-e2e-$binary-$BASE_VERSION-snapshot-$COMMIT_VERSION.tar.gz \
+        -DrepositoryId=hyperledger-snapshots \
+        -Durl=https://nexus.hyperledger.org/content/repositories/snapshots/ \
+        -DgroupId=org.hyperledger.fabric \
+        -Dversion=$binary-$BASE_VERSION-$COMMIT_VERSION-SNAPSHOT \
+        -DartifactId=fabric-binary \
+        -DgeneratePom=true \
+        -DuniqueVersion=false \
+        -Dpackaging=tar.gz \
+        -gs $GLOBAL_SETTINGS_FILE -s $SETTINGS_FILE
+     done
+     echo "========> DONE <======="
+  else
+     for binary in linux-amd64 windows-amd64 darwin-amd64 linux-ppc64le linux-s390x; do
+       TMP=$(echo "$binary" | tr -d '-')
+       mkdir -p release/$binary/chaincode/go/chaincode_example02 release/$binary/chaincode/go/marbles02
+       cp $FABRIC_ROOT_DIR/examples/chaincode/go/chaincode_example02/chaincode_example02.go release/$binary/chaincode/go/chaincode_example02/
+       cp $FABRIC_ROOT_DIR/examples/chaincode/go/marbles02/marbles_chaincode.go release/$binary/chaincode/go/marbles02/
+       cp -ar examples/e2e_cli/. release/$binary && rm -rf release/$binary/examples && sed -i "s/e2ecli/$TMP/g" release/$binary/base/peer-base.yaml && tar -czf fabric-e2e-$binary-$BASE_VERSION.tar.gz release/$binary release/$binary/chaincode
+       echo "Pushing fabric-e2e-$binary-$BASE_VERSION.tar.gz to maven releases..."
+       mvn org.apache.maven.plugins:maven-deploy-plugin:deploy-file \
+        -Dfile=$WORKSPACE/gopath/src/github.com/hyperledger/fabric/fabric-e2e-$binary-$BASE_VERSION.tar.gz \
+        -DrepositoryId=hyperledger-releases \
+        -Durl=https://nexus.hyperledger.org/content/repositories/releases/ \
+        -DgroupId=org.hyperledger.fabric \
+        -Dversion=$binary-$BASE_VERSION \
+        -DartifactId=fabric-binary \
+        -DgeneratePom=true \
+        -DuniqueVersion=false \
+        -Dpackaging=tar.gz \
+        -gs $GLOBAL_SETTINGS_FILE -s $SETTINGS_FILE
+     done
+     echo "========> DONE <======="
+fi
+
