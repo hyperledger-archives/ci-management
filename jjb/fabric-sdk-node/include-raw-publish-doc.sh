bead254eb101968372c1cad5166a90209a7cd2e3
rameshbabu <rameshbabu.thoomu@gmail.com>
2017-05-23 21:25:40 +0000

    [FAB-4119] delete doc ref from gh-pages before copy
    Delete gulp doc references from fabric-sdk-node.github.io repository before
    copy from source repo to targer
    
    Change-Id: Icd547a1d821fcd3872bfedd2a09fb7990c32841d
    Signed-off-by: rameshbabu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric-sdk-node/include-raw-publish-doc.sh
+++ jjb/fabric-sdk-node/include-raw-publish-doc.sh
@@ -8,6 +8,9 @@ SDK_COMMIT=$(git rev-parse --short HEAD)
 TARGET_REPO=$NODE_SDK_USERNAME.github.io.git
 git clone https://github.com/$NODE_SDK_USERNAME/$TARGET_REPO
 
+# Remove API docs to Target repository
+rm -rf $NODE_SDK_USERNAME.github.io/*
+
 # Copy API docs to Target repository
 cp -r docs/gen/* $NODE_SDK_USERNAME.github.io
 cd $NODE_SDK_USERNAME.github.io
