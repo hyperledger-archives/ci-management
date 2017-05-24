812e6fc6e6b71d1ece54b5b109c6174d89c3d3bf
rameshbabu <rameshbabu.thoomu@gmail.com>
2017-05-24 19:24:54 +0000

    [FAB-3913] remove unused containers, images and files
    This patch removes unused docker containers, images and tmp/hfc
    files before start the build.
    
    Change-Id: I90ba52e895426e6bb6fc7c3c9ede6fe23e7bc43b
    Signed-off-by: rameshbabu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric/include-raw-fabric-clean-environment.sh
+++ jjb/fabric/include-raw-fabric-clean-environment.sh
@@ -2,6 +2,32 @@
 
 cd gopath/src/github.com/hyperledger/fabric
 
-docker rm -f "$(docker ps -aq)" || true
+function clearContainers () {
+        CONTAINER_IDS=$(docker ps -aq)
+        if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" = " " ]; then
+                echo "---- No containers available for deletion ----"
+        else
+                docker rm -f $CONTAINER_IDS || true
+                docker ps -a
+        fi
+}
+
+function removeUnwantedImages() {
+        DOCKER_IMAGE_IDS=$(docker images | grep "dev\|none\|test-vp\|peer[0-9]-" | awk '{print $3}')
+        if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" = " " ]; then
+                echo "---- No images available for deletion ----"
+        else
+                docker rmi -f $DOCKER_IMAGE_IDS || true
+                docker images
+        fi
+}
+
 make dist-clean || true
-docker rmi -f "$(docker images | grep -v 'hyperledger/fabric-base*' | awk "{'print $3'}")" || true
+
+# remove orderer block and other channel configuration transactions and certs
+rm -rf channel-artifacts/*.block channel-artifacts/*.tx crypto-config
+
+# remove tmp/hfc and hfc-key-store data
+rm -rf /tmp/hfc* /tmp/npm* /home/jenkins/kvsTemp /home/jenkins/.hfc-key-store
+
+rm -rf /var/hyperledger/*
