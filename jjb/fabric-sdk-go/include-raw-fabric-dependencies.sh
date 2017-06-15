ad724c64e2de28ad6e2be320e140fe5f97fa63bb
Divyank Katira <Divyank.Katira@securekey.com>
2017-06-15 12:45:48 -0400

    [FAB-4645] Add parameter to allow custom images
    We can now optionally allow builds against non-tagged fabric,
    fabric-ca versions
    
    Change-Id: I264ffcc00986dd50fd9c32812cc29be9783f26ca
    Signed-off-by: Divyank Katira <Divyank.Katira@securekey.com>

--- jjb/fabric-sdk-go/include-raw-fabric-dependencies.sh
+++ jjb/fabric-sdk-go/include-raw-fabric-dependencies.sh
@@ -1,6 +1,54 @@
 #!/bin/bash -exu
 set -o pipefail
 
+source ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-sdk-go/test/scripts/fabric_test_commitlevel.sh
+
+if [ "$USE_PREBUILT_IMAGES" == false ]
+then
+  echo "Building custom images: Fabric commit is $FABRIC_COMMIT , Fabric-ca commit is $FABRIC_CA_COMMIT"
+  # Clone fabric git repository
+  #############################
+
+  rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric
+
+  WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric"
+  REPO_NAME=fabric
+  git clone ssh://hyperledger-jobbuilder@gerrit.hyperledger.org:29418/$REPO_NAME $WD
+  cd $WD
+  if [ "$FABRIC_COMMIT" == "latest" ]; then
+  echo "Fabric commit is $FABRIC_COMMIT so go with this"
+  else
+  git checkout $FABRIC_COMMIT
+  FABRIC_COMMIT_LEVEL=$(git log -1 --pretty=format:"%h")
+  fi
+  FABRIC_COMMIT_LEVEL=$(git log -1 --pretty=format:"%h")
+  echo "=======>" "FABRIC COMMIT NUMBER" "-" $FABRIC_COMMIT_LEVEL
+  # Build fabric Docker images
+  make docker
+  docker images | grep hyperledger
+
+  # Clone fabric-ca git repository
+  ################################
+
+  rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-ca
+
+  WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-ca"
+  CA_REPO_NAME=fabric-ca
+  git clone ssh://hyperledger-jobbuilder@gerrit.hyperledger.org:29418/$CA_REPO_NAME $WD
+  cd $WD
+  if [ "$FABRIC_CA_COMMIT" == "latest" ]; then
+  echo "Fabric_CA commit is $FABRIC_COMMIT so go with this"
+  else
+  git checkout $FABRIC_CA_COMMIT
+  CA_COMMIT_LEVEL=$(git log -1 --pretty=format:"%h")
+  fi
+  CA_COMMIT_LEVEL=$(git log -1 --pretty=format:"%h")
+  echo "=======>" "FABRIC CA COMMIT NUMBER" "-" $CA_COMMIT_LEVEL
+  # Build CA Docker Images
+  make docker
+  docker images | grep hyperledger
+fi
+
 # Move to fabric-sdk-go repository and execute integration tests
 export WD=${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-sdk-go
 cd $WD
