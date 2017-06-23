7f0ff1f3a66e251a437156ba8609989353cafce6
rameshbabu <rameshbabu.thoomu@gmail.com>
2017-06-23 13:46:43 +0000

    fix fabric-binary tar location error
    script is unable to pull PROJECT_VERSION. Added some logic
    to identify the PROJECT_VERSION based on the conditions mentioned
    in the fabric Makefile.
    
    Change-Id: I2add3508f1e6f50fe4d572c4dd77ca0e9d6b3163
    Signed-off-by: rameshbabu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric/include-raw-fabric-push-fabric-binaries.sh
+++ jjb/fabric/include-raw-fabric-push-fabric-binaries.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -o pipefail
 
-echo "=======>"
-echo "=======>"
 echo
 echo "Publish fabric binaries"
 echo
@@ -12,18 +10,28 @@ cd $FABRIC_ROOT_DIR || exit
 make release-clean dist-clean dist-all
 
 BASE_VERSION=`cat Makefile | grep BASE_VERSION | awk '{print $3}' | head -1`
-echo "=============> $BASE_VERSION"
-
+echo "=======> $BASE_VERSION"
 IS_RELEASE=`cat Makefile | grep IS_RELEASE | awk '{print $3}'`
 echo "=======>" $IS_RELEASE
 
+# Findout PROJECT_VERSION
+
+if [ $IS_RELEASE != "true" ]; then
+EXTRA_VERSION=snapshot-$(git rev-parse --short HEAD)
+PROJECT_VERSION=$BASE_VERSION-$EXTRA_VERSION
+echo "=======>" $PROJECT_VERSION
+else
+PROJECT_VERSION=$BASE_VERSION
+echo "=======>" $PROJECT_VERSION
+fi
+
+# Push fabric-binaries to nexus2
 if [ "${IS_RELEASE}" == "false" ]; then
 
-# copy byfn folder to release/$binary
      for binary in linux-amd64 windows-amd64 darwin-amd64 linux-ppc64le linux-s390x; do
        echo "Pushing hyperledger-fabric-$binary.$PROJECT_VERSION.tar.gz to maven snapshots..."
        mvn org.apache.maven.plugins:maven-deploy-plugin:deploy-file \
-        -Dfile=$WORKSPACE/gopath/src/github.com/hyperledger/fabric/hyperledger-fabric-$binary.$PROJECT_VERSION.tar.gz \
+        -Dfile=$WORKSPACE/gopath/src/github.com/hyperledger/fabric/release/$binary/hyperledger-fabric-$binary.$PROJECT_VERSION.tar.gz \
         -DrepositoryId=hyperledger-snapshots \
         -Durl=https://nexus.hyperledger.org/content/repositories/snapshots/ \
         -DgroupId=org.hyperledger.fabric \
@@ -39,7 +47,7 @@ if [ "${IS_RELEASE}" == "false" ]; then
      for binary in linux-amd64 windows-amd64 darwin-amd64 linux-ppc64le linux-s390x; do
        echo "Pushing hyperledger-fabric-$binary.$PROJECT_VERSION.tar.gz to maven releases.."
        mvn org.apache.maven.plugins:maven-deploy-plugin:deploy-file \
-        -Dfile=$WORKSPACE/gopath/src/github.com/hyperledger/fabric/hyperledger-fabric-$binary.$PROJECT_VERSION.tar.gz \
+        -Dfile=$WORKSPACE/gopath/src/github.com/hyperledger/fabric/release/$binary/hyperledger-fabric-$binary.$PROJECT_VERSION.tar.gz \
         -DrepositoryId=hyperledger-releases \
         -Durl=https://nexus.hyperledger.org/content/repositories/releases/ \
         -DgroupId=org.hyperledger.fabric \
