22445e7537ee4f23742ac06a84d1119f84ed8f06
rameshbabu <rameshbabu.thoomu@gmail.com>
2017-06-27 17:56:13 +0000

    FAB-5036 update daily test suite CI script
    This patch fix enables behave tests to run in virtualenv
    and modified cron job to daily instead of once in two days.
    
    It also skips the behave tests to run outside virtualenv
    
    Change-Id: I246ab1c7fd12c123d2c7997b6854f308dde85505
    Signed-off-by: rameshbabu <rameshbabu.thoomu@gmail.com>

---
+++ jjb/fabric/include-raw-fabric-behave-tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -eu
+set -o pipefail
+
+cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric
+
+# Create virtual Environment to test behave tests
+
+virtualenv -p /usr/bin/python2.7 venv
+export PS1="test"
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7
+source venv/bin/activate
+
+# Install all required python modules
+
+pip install behave
+pip install grpcio-tools
+pip install "pysha3==1.0b1"
+pip install b3j0f.aop
+pip install jinja2
+pip install pyopenssl
+pip install ecdsa
+pip install python-slugify
+pip install pyyaml
+
+# Build Peer and images
+make docker && make native
+docker images | grep hyperledger
+cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric/test/feature
+behave --junit --junit-directory test/regression/daily/. --tags=-skip --tags=daily
+cd -
+deactivate
