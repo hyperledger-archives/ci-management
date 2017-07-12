f1c271b791e5f176a4ff4f9e7f1d2a653bcaf8da
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-07-12 14:50:42 +0000

    Update release test configuration
    updated CI script to pull published docker images and capture
    build artifacts
    
    Change-Id: I462f8a816d9b67057a10d1b0087fc377c9b59d0e
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

---
+++ jjb/fabric/include-raw-fabric-release-docker-images.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -eu
+set -o pipefail
+
+# Clone fabric git repository
+#############################
+
+rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric
+
+WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric"
+REPO_NAME=fabric
+git clone ssh://hyperledger-jobbuilder@gerrit.hyperledger.org:29418/$REPO_NAME $WD
+cd $WD
+curl -sSL https://goo.gl/iX9dek | bash
+docker images | grep hyperledger
