3188741b15c720ee7876c4afb3076a80b508501b
Jessica Wagantall <jwagantall@linuxfoundation.org>
2017-07-17 12:03:11 -0700

    Fix previous yamllint issues
    Use 'cd ... || exit' or 'cd ... || return'
    in case cd fails. [SC2164]
    
    Change-Id: I08d042ef592258bc673b32ee81211c888107725c
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>

--- jjb/fabric-sdk-node/include-raw-gulp-test.sh
+++ jjb/fabric-sdk-node/include-raw-gulp-test.sh
@@ -8,7 +8,7 @@ rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric
 WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric"
 REPO_NAME=fabric
 git clone ssh://hyperledger-jobbuilder@gerrit.hyperledger.org:29418/$REPO_NAME $WD
-cd $WD
+cd $WD || exit
 #sed -i -e 's/127.0.0.1:7050\b/'"orderer:7050"'/g' $WD/common/configtx/tool/configtx.yaml
 FABRIC_COMMIT=$(git log -1 --pretty=format:"%h")
 echo "=======> FABRIC_COMMIT <======= $FABRIC_COMMIT"
@@ -23,14 +23,14 @@ rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-ca
 WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-ca"
 CA_REPO_NAME=fabric-ca
 git clone ssh://hyperledger-jobbuilder@gerrit.hyperledger.org:29418/$CA_REPO_NAME $WD
-cd $WD
+cd $WD || exit
 CA_COMMIT=$(git log -1 --pretty=format:"%h")
 echo "======> CA_COMMIT <======= $CA_COMMIT"
 make docker
 docker images | grep hyperledger
 
 ## Test gulp test
-cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-sdk-node/test/fixtures
+cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-sdk-node/test/fixtures || exit
 docker-compose up >> dockerlogfile.log 2>&1 &
 sleep 30
 docker ps -a
