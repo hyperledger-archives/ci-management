a5f4574e4ff8afff24d5fd4be19cf3e7bc0b3cfb
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-08-08 16:39:25 -0400

    Update fabric-byfn CI test script
    update fabric-byfn first network script to build docker images
    on latest fabric commits and copy bin directory to fabric-samples/
    directory.
    
    This works on both custom and default channel.
    
    Change-Id: I4dcb75485ba283454f9be3388a3e6834044181e0
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric-samples/include-raw-fabric-samples-byfn-e2e.sh
+++ jjb/fabric-samples/include-raw-fabric-samples-byfn-e2e.sh
@@ -1,32 +1,34 @@
 #!/bin/bash -eu
+
 set -o pipefail
 
-# RUN END-to-END Test
-#####################
+# RUN END-to-END Tests
+######################
 rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-samples
 
-WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-samples"
+SAMPLES_WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-samples"
 REPO_NAME=fabric-samples
+WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric"
 
-git clone ssh://hyperledger-jobbuilder@gerrit.hyperledger.org:29418/$REPO_NAME $WD
-cd $WD
-#cp -r ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric/release/linux-amd64/bin .
+git clone ssh://hyperledger-jobbuilder@gerrit.hyperledger.org:29418/$REPO_NAME $SAMPLES_WD
+cd $WD && make release
 
-curl -L https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap-1.0.0.sh -o bootstrap-1.0.0.sh
-chmod +x bootstrap-1.0.0.sh
-./bootstrap-1.0.0.sh
+cp -r $WD/release/linux-amd64/bin/ $SAMPLES_WD
 
-cd $WD/first-network
-export PATH=$WD/bin:$PATH
-#
-echo "=======> DEFAULUT CHANNEL"
+cd $SAMPLES_WD/first-network
+export PATH=$SAMPLES_WD/bin:$PATH
+
+echo
+echo "======> DEFAULT CHANNEL <======"
+
+echo y | ./byfn.sh -m down
 echo y | ./byfn.sh -m generate
-echo y | ./byfn.sh -m up -t 10
+echo y | ./byfn.sh -m up -t 10 && docker ps -a && docker logs -f cli | tee default_channel.log
 echo y | ./byfn.sh -m down
 
-#
-echo "=======> CUSTOM CHANNEL"
+echo
+echo "======> CUSTOM CHANNEL <======="
 
 echo y | ./byfn.sh -m generate -c fabricrelease
-echo y | ./byfn.sh -m up -c fabricrelease -t 10
+echo y | ./byfn.sh -m up -c fabricrelease -t 10 && docker ps -a && docker logs -f cli | tee custom_channel.log
 echo y | ./byfn.sh -m down
