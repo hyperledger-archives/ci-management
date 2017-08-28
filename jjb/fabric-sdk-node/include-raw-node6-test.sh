3800f1b5c0917eb2e350a3e940df9dfd773c1d9a
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-08-28 16:54:19 -0400

    Add fabric-sdk-node CI job to test on node8.4.0
    Create a new job to test on node 8.4.0 and update configuration
    to test on 6.9.5 node js version.
    
    existing node jobs on s390x continue as is for now, once install
    nvm, will update CI configuration.
    
    Change-Id: If4f06fb84c6be639a1e2b25f495235cbacb048a8
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

---
+++ jjb/fabric-sdk-node/include-raw-node6-test.sh
@@ -0,0 +1,66 @@
+#!/bin/bash -e
+
+# Clone fabric git repository
+#############################
+
+rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric
+
+WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric"
+REPO_NAME=fabric
+git clone ssh://hyperledger-jobbuilder@gerrit.hyperledger.org:29418/$REPO_NAME $WD
+cd $WD || exit
+#sed -i -e 's/127.0.0.1:7050\b/'"orderer:7050"'/g' $WD/common/configtx/tool/configtx.yaml
+FABRIC_COMMIT=$(git log -1 --pretty=format:"%h")
+echo "=======> FABRIC_COMMIT <======= $FABRIC_COMMIT"
+make docker
+docker images | grep hyperledger
+
+# Clone fabric-ca git repository
+################################
+
+rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-ca
+
+WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-ca"
+CA_REPO_NAME=fabric-ca
+git clone ssh://hyperledger-jobbuilder@gerrit.hyperledger.org:29418/$CA_REPO_NAME $WD
+cd $WD || exit
+CA_COMMIT=$(git log -1 --pretty=format:"%h")
+echo "======> CA_COMMIT <======= $CA_COMMIT"
+make docker
+docker images | grep hyperledger
+
+## Test gulp test
+cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-sdk-node/test/fixtures || exit
+docker-compose up >> dockerlogfile.log 2>&1 &
+sleep 30
+docker ps -a
+
+cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-sdk-node || exit
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+# shellcheck source=/dev/null
+export NVM_DIR="$HOME/.nvm"
+# shellcheck source=/dev/null
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install 6.9.5 || true
+nvm use 6.9.5
+npm -v
+node -v
+npm install
+npm config set prefix ~/npm && npm install -g gulp && npm install -g istanbul
+gulp || exit 1
+gulp ca || exit 1
+rm -rf node_modules/fabric-ca-client && npm install
+gulp test
+
+# copy debug log file to $WORKSPACE directory
+
+if [ $? == 0 ]; then
+
+# Copy Debug log to $WORKSPACE
+cp /tmp/hfc/test-log/*.log $WORKSPACE
+else
+# Copy Debug log to $WORKSPACE
+cp /tmp/hfc/test-log/*.log $WORKSPACE
+exit 1
+
+fi
