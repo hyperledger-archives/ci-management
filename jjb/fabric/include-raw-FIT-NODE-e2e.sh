c4c1a0d5aca9f022e56f22a4d63f1808dd1d461b
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-09-11 13:52:47 -0400

    FAB-6106 update e2e node CI scripts
    update node e2e CI scripts to install npm versions and
    update properties content file to accept npm path globally
    
    Change-Id: Idee3291c5095f49e551a90e146843b9d1213f152
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric/include-raw-FIT-NODE-e2e.sh
+++ jjb/fabric/include-raw-FIT-NODE-e2e.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eu
+#!/bin/bash -e
 set -o pipefail
 
 # RUN END-to-END Test
@@ -16,9 +16,35 @@ docker rm -f "$(docker ps -aq)" || true
 docker-compose up >> node_dockerlogfile.log 2>&1 &
 sleep 10
 docker ps -a
-cd ../.. && npm install
-npm config set prefix ~/npm && npm install -g gulp && npm install -g istanbul
-gulp && gulp ca
+cd ../..
+
+# Install nvm to install multi node versions
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+# shellcheck source=/dev/null
+export NVM_DIR="$HOME/.nvm"
+# shellcheck source=/dev/null
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+# Install nodejs version 6.9.5
+nvm install 6.9.5 || true
+
+# use nodejs 6.9.5 version
+nvm use 6.9.5
+
+echo "npm version ======>"
+npm -v
+echo "node version =======>"
+node -v
+
+npm install && npm config set prefix ~/npm && npm install -g gulp && npm install -g istanbul
+gulp || exit 1
+gulp ca || exit 1
 rm -rf node_modules/fabric-ca-client && npm install
+
+# Execute unit test and code coverage
+echo "############"
+echo "Run e2e tests and Code coverage report"
+echo "############"
+
 istanbul cover --report cobertura test/integration/e2e.js
+
 docker rm -f "$(docker ps -aq)" || true
