b6eb001f2425017377dab5374df2137456c06d43
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-09-13 09:49:54 -0400

    FAB-5547 add fabric-test daily job
    Create fabric-test daily cron job to run behave daily tag based
    test cases. Once the "runDailyTestSuite.sh" script is ready to use, will
    include "behave -t daily" and above script to test daily tests.
    
    Change-Id: I9ee517635da5fd6ca827612f9143e861ff0e8784
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

---
+++ jjb/fabric/include-raw-fabric-test-behave-daily.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -eu
+set -o pipefail
+
+cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-test
+
+git submodule update --init --recursive
+
+# Create virtual Environment to test behave tests
+
+virtualenv -p /usr/bin/python2.7 venv
+export PS1="test"
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7
+source venv/bin/activate
+
+# Install all required python modules
+
+pip install behave
+pip install grpcio-tools
+pip install "pysha3==1.0b1"
+pip install b3j0f.aop
+pip install jinja2
+pip install pyopenssl
+pip install ecdsa
+pip install python-slugify
+pip install pyyaml
+pip install pykafka
+
+# Execute behave smoke tests
+cd feature
+behave --junit --junit-directory . -t daily
+cd -
+deactivate
