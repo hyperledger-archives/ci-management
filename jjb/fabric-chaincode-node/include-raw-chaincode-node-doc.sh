8b91201d036b1da4a1baf0e61ce3b6ecf17c01e5
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-10-05 17:50:09 -0400

    FAB-5444 publish chaincode-node API docs
    This patch publishes fabric-chaincode-node API docs to
    fabric-shim.github.io after every successful merge of chaincode-node CI
    job
    
    Change-Id: I06d2a10218234a160b352845c4cc8d52b9924e9b
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

---
+++ jjb/fabric-chaincode-node/include-raw-chaincode-node-doc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -eu
+set -o pipefail
+
+cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-chaincode-node
+# Generate chaincode-node API docs
+gulp doc
+CHAINCODE_NODE_COMMIT=$(git rev-parse --short HEAD)
+TARGET_REPO=$CHAINCODE_NODE_USERNAME.github.io.git
+git clone https://github.com/$CHAINCODE_NODE_USERNAME/$TARGET_REPO
+
+# Remove API docs from Target repository
+rm -rf $CHAINCODE_NODE_USERNAME.github.io/*
+
+# Copy API docs to Target repository
+cp -r docs/gen/* $CHAINCODE_NODE_USERNAME.github.io
+cd $CHAINCODE_NODE_USERNAME.github.io
+git add .
+git commit -m "CHAINCODE_NODE commit - $CHAINCODE_NODE_COMMIT"
+git config remote.gh-pages.url https://$CHAINCODE_NODE_USERNAME:$CHAINCODE_NODE_PASSWORD@github.com/$CHAINCODE_NODE_USERNAME/$TARGET_REPO
+
+# Push API docs to Target repository
+git push gh-pages master
