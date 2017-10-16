e5eb36f6f1dae25f1ab4ef93db73967156ff47d2
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-10-16 12:31:53 -0400

    FAB-6204 vulnerability checks for fabric-sdk-node
    added vulnerability checks for packages that fabric-sdk-node depends on
    
    Change-Id: I52cd2509a5a19431eaf8d6a53b2c6c014166d646
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

---
+++ jjb/fabric-sdk-node/include-raw-vulnerable-tests.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+set -o pipefail
+
+## Test fabric-sdk-node tests
+################################
+
+cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-sdk-node || exit
+
+# Install nvm to install multi node versions
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+# shellcheck source=/dev/null
+export NVM_DIR="$HOME/.nvm"
+# shellcheck source=/dev/null
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+# Install nodejs version 8.4.0
+nvm install 8.4.0 || true
+
+# use nodejs 8.4.0 version
+nvm use 8.4.0
+
+echo "npm version ======>"
+npm -v
+echo "node version =======>"
+node -v
+
+npm install
+npm config set prefix ~/npm && npm install -g nsp
+cd fabric-client && nsp check --output summary
+
+if [ $? -eq 0 ] ; then
+
+     echo " ===> PASS !!! No vulnerable errors found on fabric-client"
+     echo " ===> Execute vulnerable tests on fabric-ca-client"
+
+cd ../fabric-ca-client && nsp check --output summary
+
+if [ $? -eq 0 ] ; then
+     echo " ===> PASS !!! No vulnerable errors found on fabric-ca-client"
+     echo
+  else
+     echo " ===> ERROR!! Found vulnerable errors found"
+
+fi
+fi
+
