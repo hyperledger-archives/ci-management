17c2650947771125ecc39a94cc17bf989f83240f
Jessica Wagantall <jwagantall@linuxfoundation.org>
2017-10-18 09:00:45 -0700

    Install python-tox package on baseline images
    Change-Id: I1ec37cf61c4c6a84643016da61c0817a6bc125c8
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>

--- packer/provision/baseline.sh
+++ packer/provision/baseline.sh
@@ -80,7 +80,8 @@ EOF
     # separate group installs from package installs since a non-existing
     # group with dnf based systems (F21+) will fail the install if such
     # a group does not exist
-    yum install -y -q unzip xz puppet git git-review perl-XML-XPath xmlstarlet facter maven libltdl-dev crudini
+    yum install -y -q unzip xz puppet git git-review perl-XML-XPath xmlstarlet facter \
+                      maven libltdl-dev python-tox crudini
 
     # All of our systems require Java (because of Jenkins)
     # Install all versions of the OpenJDK devel but force 1.7.0 to be the
@@ -165,7 +166,7 @@ EOF
     # add in stuff we know we need
     echo "---> Installing base packages"
     apt-get install unzip xz-utils puppet git git-review libxml-xpath-perl \
-                    xmlstarlet facter maven libltdl-dev crudini
+                    xmlstarlet facter maven libltdl-dev python-tox crudini
 
     # install Java 7
     echo "---> Configuring OpenJDK"
