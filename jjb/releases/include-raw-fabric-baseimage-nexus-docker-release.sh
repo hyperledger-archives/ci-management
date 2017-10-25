558e0857500ac15f5bd3e6dc26f929741d8d7d7f
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-10-25 10:44:45 -0400

    FAB-6743 publish fabric-baseimages to nexus docker
    publish fabric-baseimage, fabric-baseos and fabric-basejvm images
    to nexus docker repository on every release tag is created in
    fabric-baseimage.
    
    Change-Id: I1e3fe31a5d268832d4ba233c2948cdfdf082f0d1
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

---
+++ jjb/releases/include-raw-fabric-baseimage-nexus-docker-release.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o pipefail
+
+BASE_TAG=$(docker inspect --format "{{ .RepoTags }}" hyperledger/fabric-baseimage | sed 's/.*:\(.*\)]/\1/')
+echo "======> $BASE_TAG"
+NEXUS_URL=nexus3.hyperledger.org:10003
+ORG_NAME="hyperledger/fabric"
+
+# Push docker images to nexus docker repository
+
+dockerBasePush() {
+
+  for IMAGES in baseos basejvm baseimage; do
+    echo "==> $IMAGES"
+    echo
+    docker tag $ORG_NAME-$IMAGES:latest $NEXUS_URL/$ORG_NAME-$IMAGES:$BASE_TAG
+    echo "==> $NEXUS_URL/$ORG_NAME-$IMAGES:$BASE_TAG"
+    docker push $NEXUS_URL/$ORG_NAME-$IMAGES:$BASE_TAG
+    echo
+    echo "==> $NEXUS_URL/$ORG_NAME-$IMAGES:$BASE_TAG"
+    echo
+  done
+}
+
+dockerBasePush
+
+# Listout all docker images After push to NEXUS Docker
+docker images | grep "hyperledger*"
--- jjb/releases/include-raw-fabric-ca-nexus-docker-release.sh
+++ jjb/releases/include-raw-fabric-ca-nexus-docker-release.sh
@@ -3,7 +3,8 @@
 set -o pipefail
 
 CA_TAG=$(docker inspect --format "{{ .RepoTags }}" hyperledger/fabric-ca | sed 's/.*:\(.*\)]/\1/')
-
+echo "========> $CA_TAG"
+echo
 NEXUS_URL=nexus3.hyperledger.org:10003
 ORG_NAME="hyperledger/fabric"
 
@@ -14,6 +15,8 @@ dockerCaPush() {
   # shellcheck disable=SC2043
   for IMAGES in ca ca-peer ca-orderer ca-tools; do
     echo "==> $IMAGES"
+    docker tag $ORG_NAME-$IMAGES:latest $NEXUS_URL/$ORG_NAME-$IMAGES:$CA_TAG
+    echo "==> $NEXUS_URL/$ORG_NAME-$IMAGES:$CA_TAG"
     docker push $NEXUS_URL/$ORG_NAME-$IMAGES:$CA_TAG
     echo
     echo "==> $NEXUS_URL/$ORG_NAME-$IMAGES:$CA_TAG"
