c212e14a7452adc921a762467e9d83da375e2cdc
Sambhav Nidamarty <sambhavdutt@gmail.com>
2017-11-16 14:29:19 -0500

    [FAB-6995] Push latest stable images to nexus3
    Publish latest stable fabric and fabric-ca docker images to nexus3
    docker repository after successfully execute the daily test suite. Once
    the tests are executed, tag existing docker images to DEV_STABLE tag and
    push it to nexus.
    
    Once the images are successfully executed, will update docker images
    to pull these images from nexus.
    
    Change-Id: Ib8b0a3560a8324ac9267ee70d6b83b963ebfe35c
    Signed-off-by: Sambhav Nidamarty <sambhavdutt@gmail.com>

---
+++ jjb/fabric/include-raw-nexus-stable-docker-push.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -eu
+
+set -o pipefail
+
+FABRIC_TAG=DEV_STABLE
+CA_TAG=DEV_STABLE
+
+NEXUS_URL=nexus3.hyperledger.org:10003
+ORG_NAME="hyperledger/fabric"
+# tag fabric images to nexusrepo
+
+dockerTag() {
+  for IMAGES in peer orderer couchdb ccenv javaenv kafka zookeeper tools; do
+    echo "==> $IMAGES"
+    echo
+    docker tag $ORG_NAME-$IMAGES:latest $NEXUS_URL/$ORG_NAME-$IMAGES:$FABRIC_TAG
+    echo "==> $NEXUS_URL/$ORG_NAME-$IMAGES:$FABRIC_TAG"
+  done
+}
+dockerCaTag() {
+  echo
+  docker tag $ORG_NAME-ca:latest $NEXUS_URL/$ORG_NAME-ca:$CA_TAG
+  echo "==> $NEXUS_URL/$ORG_NAME-ca:$CA_TAG"
+}
+# Push docker images to nexus repository
+
+dockerFabricPush() {
+  for IMAGES in peer orderer couchdb ccenv javaenv kafka zookeeper tools; do
+    echo "==> $IMAGES"
+    docker push $NEXUS_URL/$ORG_NAME-$IMAGES:$FABRIC_TAG
+    echo
+    echo "==> $NEXUS_URL/$ORG_NAME-$IMAGES:$FABRIC_TAG"
+  done
+}
+
+dockerCaPush() {
+  docker push $NEXUS_URL/$ORG_NAME-ca:$CA_TAG
+  echo
+  echo "==> $NEXUS_URL/$ORG_NAME-ca:$CA_TAG"
+}
+
+# Tag Fabric Docker Images to Nexus Repository
+dockerTag
+
+# Push Fabric Docker Images to Nexus Repository
+dockerFabricPush
+
+# Tag Fabric-ca Docker Image to Nexus Repository
+dockerCaTag
+
+# Push Fabric-ca docker images to nexus Repository
+dockerCaPush
+
+# Listout all docker images Before and After Push to NEXUS
+docker images | grep "nexus*"
