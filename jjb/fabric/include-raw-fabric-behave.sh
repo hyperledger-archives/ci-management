0278b3b78007f9bdcddf0dfb662e9df9dc9dfd0c
rameshthoomu <rameshbabu.thoomu@gmail.com>
2018-01-09 14:23:33 -0500

    FAB-7677 cleanup behave test script
    this patch deletes behave test script also rename fabric
    smoke daily test name
    
    Change-Id: I24d7aa9d87f4f33e278570c1a059f441f9b68106
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric/include-raw-fabric-behave.sh
+++ jjb/fabric/include-raw-fabric-behave.sh
@@ -1,38 +1,7 @@
 #!/bin/bash -eu
 set -o pipefail
 
-cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric
-
-# Create virtual Environment to test behave tests
-
-virtualenv -p /usr/bin/python2.7 venv
-export PS1="test"
-export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7
-source venv/bin/activate
-
-# Install all required python modules
-
-pip install behave
-pip install grpcio-tools
-pip install "pysha3==1.0b1"
-pip install b3j0f.aop
-pip install jinja2
-pip install "pyopenssl==17.2.0"
-pip install ecdsa
-pip install python-slugify
-pip install pyyaml
-pip install pykafka
-pip install requests
-pip install pyexecjs
-pip install cython
-pip install pyjnius
-
-# Build Peer and images
-
-make behave-deps
-docker images | grep hyperledger
-cd bddtests
+make behave-deps -C ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric
+cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric/bddtests && docker images | grep "hyperledger"
 behave -k -D logs=force -D cache-deployment-spec
 #behave -k -D cache-deployment-spec features/bootstrap.feature
-# Deactivate vitrualenv after behave test
-deactivate
