cfe2491859f4b9c0a69131235c0e50e994eb4913
ramesh <rameshbabu.thoomu@gmail.com>
2018-01-25 12:40:26 -0500

    update release version in release tests
    update fabric latest release version to 1.1.0-alpha
    
    Change-Id: Id60ee5cdbf420627ddca3a2c693340cfa33943f3
    Signed-off-by: ramesh <rameshbabu.thoomu@gmail.com>

--- jjb/fabric/include-raw-fabric-release-docker-images.sh
+++ jjb/fabric/include-raw-fabric-release-docker-images.sh
@@ -10,6 +10,6 @@ WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric"
 REPO_NAME=fabric
 git clone ssh://hyperledger-jobbuilder@gerrit.hyperledger.org:29418/$REPO_NAME $WD
 cd $WD
-git checkout tags/v1.1.0-preview
-curl -sSL https://goo.gl/fMh2s3 | bash
+git checkout tags/v1.1.0-alpha
+curl -sSL https://goo.gl/6wtTN5 | bash -s 1.1.0-alpha
 docker images | grep hyperledger
