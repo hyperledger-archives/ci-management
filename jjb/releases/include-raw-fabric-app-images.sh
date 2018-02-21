1f9e450e59a67f5bf9a6b4000049748f5e9b28b1
rameshthoomu <rameshbabu.thoomu@gmail.com>
2018-02-20 23:37:21 -0500

    push thirdparty docker images for 1.0.x releases
    We are using the same release scheme for 1.0 and 1.1, but the
    scheme only works properly for 1.1 because of the fact that we
    publish the kafka, couch, etc images from baseimage now.
    
    As a work around, this patch pushes the docker images based on the
    GERRIT_REFSPEC value. If the value is 1.0.x then it publishes thirdparty
    images along with fabric images otherwise, will publish only fabric
    docker images assuming thirdparty images are published from baseimage
    repo.
    
    Change-Id: I4c663cbb6692015affb8f9d155380446e8ae82c7
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/releases/include-raw-fabric-app-images.sh
+++ jjb/releases/include-raw-fabric-app-images.sh
@@ -9,7 +9,22 @@ ORG_NAME="hyperledger/fabric"
 docker login --username=$DOCKER_HUB_USERNAME --password=$DOCKER_HUB_PASSWORD
 # Push docker images to nexus repository
 
-dockerFabricPush() {
+#TODO: this is a temporary change to push 1.0.6 thirdparty images
+#This process has to steamline or add other fabric images to the below list
+
+docker_Fabric_Thirdparty_Push() {
+
+  # shellcheck disable=SC2043
+  for IMAGES in kafka couchdb zookeeper; do
+    echo "==> $IMAGES"
+    docker push $ORG_NAME-$IMAGES:$FABRIC_TAG
+    echo
+    echo "==> $ORG_NAME-$IMAGES:$FABRIC_TAG"
+    echo
+  done
+}
+
+docker_Fabric_Push() {
 
   # shellcheck disable=SC2043
   for IMAGES in peer orderer ccenv javaenv tools; do
@@ -20,9 +35,19 @@ dockerFabricPush() {
     echo
   done
 }
+echo "-----> GERRIT_REFSPEC is: $GERRIT_REFSPEC"
+echo $GERRIT_REFSPEC | grep 'v1.0.*'
+if [ $? != '0' ]; then
 
-# Push Fabric Docker Images to Nexus Repository
-dockerFabricPush
-
+     # Push Fabric Docker Images
+     echo "-----> Release tag: $GERRIT_REFSPEC"
+     echo "-----> Pushing fabric and thirdparty docker imges"
+     docker_Fabric_Push
+else
+     # Push Fabric & Thirdparty Docker Images
+     echo "-----> Release tag: $GERRIT_REFSPEC"
+     echo "-----> Pushing fabric and thirdparty docker imges"
+     docker_Fabric_Thirdparty_Push
+fi
 # Listout all docker images Before and After Push to NEXUS
 docker images | grep "hyperledger*"
