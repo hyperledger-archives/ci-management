8a1b27c3fb5171fabe3790bd169c32533320ea24
Sambhav Nidamarty <sambhavdutt@gmail.com>
2018-02-28 17:58:53 -0500

    FAB-8219 Release tests to run BYFN,EYFN tests.
    This patch creates release jobs to run byfn, eyfn tests on
    x,p and z platforms by pulling released docker images and binaries.
    We trigger these jobs manually after making sure fabric and fabric-ca
    release jobs are successful.
    
    TODO: Will add these jobs as downstream jobs to fabric and fabric-ca
    release jobs (upstream) jobs. Once the docker images, binaries are
    published, will trigger downstream jobs to pull and test.
    
    Change-Id: I98c29afcd90f6833a4aa99ea68ad735e565943b0
    Signed-off-by: Sambhav Nidamarty <sambhavdutt@gmail.com>
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric/include-raw-fabric-byfn-daily-tests.sh
+++ jjb/fabric/include-raw-fabric-byfn-daily-tests.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -eu
-
 set -o pipefail
 
 # RUN BYFN END-to-END Tests
@@ -11,14 +10,15 @@ REPO_NAME=fabric-samples
 git clone https://gerrit.hyperledger.org/r/$REPO_NAME $WD
 cd $WD || exit
 git checkout $GERRIT_BRANCH
-BYFN_COMMIT=$(git log -1 --pretty=format:"%h")
-echo "BYFN_COMMIT ===========> $BYFN_COMMIT"
-echo "BYFN COMMIT ========> $BYFN_COMMIT" >> ${WORKSPACE}/gopath/src/github.com/hyperledger/commit.log
+echo "-------> GERRIT_BRANCH: $GERRIT_BRANCH"
+FABRIC_SAMPLES_COMMIT=$(git log -1 --pretty=format:"%h")
+echo "FABRIC_SAMPLES_COMMIT ========> $FABRIC_SAMPLES_COMMIT" >> ${WORKSPACE}/gopath/src/github.com/hyperledger/commit.log
 # copy /bin directory to fabric-samples
 cp -r ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric/release/linux-amd64/bin/ .
 
 cd first-network
 export PATH=gopath/src/github.com/hyperledger/fabric-samples/bin:$PATH
+
 # Execute below tests
 echo "############## BYFN,EYFN DEFAULT TEST####################"
 echo "#########################################################"
@@ -27,7 +27,7 @@ echo y | ./byfn.sh -m down
 echo y | ./byfn.sh -m generate
 echo y | ./byfn.sh -m up -t 60
 echo y | ./eyfn.sh -m up
-echo y | ./byfn.sh -m down
+echo y | ./eyfn.sh -m down
 echo
 echo "############## BYFN,EYFN CUSTOM CHANNEL TEST#############"
 echo "#########################################################"
@@ -43,7 +43,7 @@ echo "####################################################"
 echo y | ./byfn.sh -m generate -c couchdbtest
 echo y | ./byfn.sh -m up -c couchdbtest -s couchdb -t 60
 echo y | ./eyfn.sh -m up -c couchdbtest -s couchdb -t 60
-echo y | ./byfn.sh -m down
+echo y | ./eyfn.sh -m down
 echo
 echo "############### BYFN,EYFN NODE TEST ################"
 echo "####################################################"
---
+++ jjb/fabric/include-raw-fabric-byfn-release-tests.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# RUN BYFN & EYFN tests on latest release images and binaries
+#############################################################
+
+rm -rf ${GOPATH}/src/github.com/hyperledger/fabric-samples
+WD="${GOPATH}/src/github.com/hyperledger/fabric-samples"
+REPO_NAME=fabric-samples
+curl -L https://raw.githubusercontent.com/hyperledger/fabric/master/Makefile > Makefile
+RELEASE_VERSION=$(cat Makefile | grep "BASE_VERSION =" | awk '{print $3}')
+echo "--------> RELEASE_VERSION : $RELEASE_VERSION"
+
+# Delete temporary Makefile.
+rm -rf Makefile
+
+git clone ssh://hyperledger-jobbuilder@gerrit.hyperledger.org:29418/$REPO_NAME $WD
+cd $WD || exit
+git checkout v$RELEASE_VERSION
+curl -sSL https://goo.gl/6wtTN5 | bash -s $RELEASE_VERSION
+
+cd $WD/first-network || exit
+export PATH=$WD/bin:$PATH
+
+echo "############## BYFN,EYFN DEFAULT CHANNEL TEST#############"
+echo "#########################################################"
+echo y | ./byfn.sh -m down
+echo y | ./byfn.sh -m generate
+echo y | ./byfn.sh -m up -t 60
+echo y | ./eyfn.sh -m up
+echo y | ./eyfn.sh -m down
+echo
+echo "############## BYFN,EYFN CUSTOM CHANNEL TEST#############"
+echo "#########################################################"
+
+echo y | ./byfn.sh -m up -c fabricrelease -t 60
+echo y | ./eyfn.sh -m up -c fabricrelease -t 60
+echo y | ./eyfn.sh -m down
+echo
+echo "############### BYFN,EYFN COUCHDB TEST #############"
+echo "####################################################"
+
+echo y | ./byfn.sh -m up -c couchdbtest -s couchdb -t 60
+echo y | ./eyfn.sh -m up -c couchdbtest -s couchdb -t 60
+echo y | ./eyfn.sh -m down
+echo
+echo "############### BYFN,EYFN NODE TEST ################"
+echo "####################################################"
+
+echo y | ./byfn.sh -m up -l node -t 60
+echo y | ./eyfn.sh -m up -l node -t 60
+echo y | ./eyfn.sh -m down
