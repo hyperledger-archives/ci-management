aec66952bbb4ba5ca1f3572d5a417a31c818c12d
rameshthoomu <rameshbabu.thoomu@gmail.com>
2018-05-14 14:57:44 -0400

    FAB-10042 Update chaincode-java CI config
    Update fabric-chaincode-java CI config to ignore whitespaces
    on files generated by compiler and fail the build if .jar files
    are checked in.
    
    Change-Id: Icee917fddec8be09b94f110c42dfda5e16054ddc
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

---
+++ jjb/fabric-chaincode-java/include-raw-chaincode-java-commit-checks.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# This script makes several basic commit message validations.
+# This is with the purpose of keeping up with the aesthetics
+# of our code.
+
+# Verify if the commit message contains JIRA URLs.
+# its-jira pluggin attempts to process jira links and breaks.
+
+JIRA_LINK=`git rev-list --format=%B --max-count=1 HEAD | grep -io 'http[s]*://jira\..*'`
+if [[ ! -z "$JIRA_LINK" ]]
+then
+  echo 'Error: Remove JIRA URLs from commit message'
+  echo 'Add jira references as: Issue: <JIRAKEY>-<ISSUE#>, instead of URLs'
+  exit 1
+fi
+
+# Verify if there are trailing spaces in the files.
+
+COMMIT_FILES=$(git diff-tree --no-commit-id --name-only -r HEAD | grep -Ev '(^|/)protos/')
+JAR_FILES=$(git diff-tree --no-commit-id --name-only -r HEAD | grep '.jar')
+
+# Verify if there are any jar files checked in
+if [[ ! -z "$JAR_FILES" ]]
+then
+  echo "echo -------->JAR files are: $JAR_FILES"
+  echo 'Error: JAR file is checked in... '
+  exit 1
+fi
+
+for filename in `echo $COMMIT_FILES`; do
+  if [[ `file $filename` == *"ASCII text"* ]];
+  then
+    if [ ! -z "`egrep -l " +$" $filename`" ];
+    then
+      FOUND_TRAILING='yes'
+      echo "Error: Trailing white spaces found in file:$filename"
+    fi
+  fi
+done
+
+if [ ! -z ${FOUND_TRAILING+x} ];
+then
+  exit 1
+fi
