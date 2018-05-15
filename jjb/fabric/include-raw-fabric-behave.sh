539385b6c9bab0cf08db305ce3b8c27905e18ebc
rameshthoomu <rameshbabu.thoomu@gmail.com>
2018-05-14 21:26:56 -0400

    FAB-10053 Cleanup deadcode from CI config
    This patch deletes the deadcode from CI config and
    make configuration more clean to use
    
    Change-Id: Iaace59c6fab28b2499c99e95970eb6d6e4494bbf
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric/include-raw-fabric-behave.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eu
-set -o pipefail
-
-make behave-deps -C ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric
-cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric/bddtests && docker images | grep "hyperledger"
-behave -k -D logs=force -D cache-deployment-spec
-#behave -k -D cache-deployment-spec features/bootstrap.feature
