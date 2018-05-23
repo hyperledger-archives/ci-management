7afde2faee1d32518ba9230ca66426c950eb1a24
Jason Clark <jtclark@linux.vnet.ibm.com>
2018-05-23 17:26:36 -0400

    [FAB-9046] Adding the PTE image publish job.
    The commit adds a new CI job that triggers the
    docker build and publishing of the PTE docker
    image to nexus3.
    
    Change-Id: I7c4253ccf5fe2af647e6d6a16dc5f2a6b234a5a9
    Signed-off-by: Jason Clark <jtclark@linux.vnet.ibm.com>

---
+++ jjb/fabric-test/shell/include-raw-pte-publish.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -eu
+set -o pipefail
+
+NEXUS_URL=nexus3.hyperledger.org:10003
+ORG_NAME="hyperledger/fabric"
+TAG=$GIT_COMMIT
+STABLE_TAG=stable
+export COMMIT_TAG=${TAG:0:7}
+
+echo "----> Publishing PTE image to nexus3..."
+echo
+
+cd $WORKSPACE/gopath/src/github.com/hyperledger/fabric-test/
+
+docker build -t $ORG_NAME-pte images/PTE
+
+dockerPTETag() {
+  echo "==> $ORG_NAME-pte"
+  echo
+  docker tag $ORG_NAME-pte $NEXUS_URL/$ORG_NAME-pte:$STABLE_TAG
+  docker tag $ORG_NAME-pte $NEXUS_URL/$ORG_NAME-pte:$STABLE_TAG-$COMMIT_TAG
+  echo "==> $NEXUS_URL/$ORG_NAME-pte:$STABLE_TAG"
+}
+
+dockerPTEPush() {
+  echo "==> $ORG_NAME-pte"
+  echo
+  docker push $NEXUS_URL/$ORG_NAME-pte:$STABLE_TAG
+  docker push $NEXUS_URL/$ORG_NAME-pte:$STABLE-$COMMIT_TAG
+  echo
+  echo "==> $NEXUS_URL/$ORG_NAME-pte:$STABLE_TAG"
+}
+
+# Tag PTE Docker Images to Nexus Repository
+echo "----> Tagging PTE image for nexus3 repo..."
+echo
+dockerPTETag
+
+# Push PTE Docker Images to Nexus Repository
+echo "----> Pushing PTE image to nexus3 repo..."
+echo
+dockerPTEPush
+
+# Listout all docker images Before and After Push to NEXUS
+docker images | grep "nexus*"
