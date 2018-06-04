f682913f83fc828395184628cba345c0e399d9fe
Sambhav Nidamarty <sambhavdutt@gmail.com>
2018-06-04 09:36:42 -0400

    fabric-ca nexus release jobs.
    Create fabric-ca nexus release jobs to publish
    fabric-ca images to the Nexus repository.
    
    Change-Id: I16039ffa230f4da164f0b44f572fc9c55b0bdb8c
    Signed-off-by: Sambhav Nidamarty <sambhavdutt@gmail.com>

---
+++ jjb/fabric-ca/shell/include-raw-fabric-ca-nexus-docker-images.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -o pipefail
+
+# Build fabric-ca images
+cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-ca || exit
+
+make docker
+if [ $? != 0 ]; then
+     echo "--------> make docker failed"
+     exit 1
+fi
+
+# Listout all docker images
+docker images
+
+# Publish fabric-ca docker images
+NEXUS_URL=nexus3.hyperledger.org:10002
+ARCH=$(dpkg --print-architecture)
+VERSION=$(cat Makefile | grep "BASE_VERSION =" | awk '{print $3}')
+
+if [ $ARCH = s390x ]; then
+   echo "--------> $ARCH"
+   ARCH=s390x
+elif [[ "$GERRIT_BRANCH" = *"release-1.1"* ]]; then
+   ARCH=x86_64
+   echo "----------> ARCH:" $ARCH
+else
+   ARCH=amd64
+   echo "----------> ARCH:" $ARCH
+fi
+
+for IMAGES in ca ca-peer ca-tools ca-orderer; do
+      docker tag hyperledger/fabric-$IMAGES:$ARCH-$VERSION $NEXUS_URL/hyperledger/fabric-$IMAGES:$ARCH-$VERSION
+      docker push $NEXUS_URL/hyperledger/fabric-$IMAGES:$ARCH-$VERSION
+      echo "--------> Push fabric-ca Image version:" $NEXUS_URL/hyperledger/fabric-$IMAGES:$ARCH-$VERSION
+done
