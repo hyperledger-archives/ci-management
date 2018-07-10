d3add0004de013d98f9b8e88094f01f41b8ec8aa
rameshthoomu <rameshbabu.thoomu@gmail.com>
2018-07-10 09:23:03 -0400

    Enable fabric-ca images from daily builds
    This change enables fabric-ca images in the nightly builds
    and skip e2e_cli tests on release-1.1 and release-1.0 branches.
    
    Change-Id: I94d64f214efd07ba68fb7a0176841cfdceae63b7
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/fabric/shell/include-raw-FIT-CLI-e2e.sh
+++ jjb/fabric/shell/include-raw-FIT-CLI-e2e.sh
@@ -2,18 +2,20 @@
 set -o pipefail
 
 # Run end-to-end (CLI) Tests
-######################
+############################
 
-cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric/examples/e2e_cli
-# wait for 10 sec's before exit
-./network_setup.sh restart mychannel 10
-docker ps -a && docker logs -f cli | tee results.log && ./network_setup.sh down
-
-grep -q "All GOOD, End-2-End execution completed " results.log
-  if [ $? -ne 0 ]; then
-      echo "=============E2E TEST FAILED========="
-      exit 1
-      else
-      echo "=============E2E TEST PASSED=========="
-
+if [[ "$GERRIT_BRANCH" == "release-1.0" || "$GERRIT_BRANCH" == "release-1.1" ]]; then
+    echo "----------> SKIP e2e_cli tests FAB-11077"
+else
+    cd ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric/examples/e2e_cli
+    # wait for 10 sec's before exit
+    ./network_setup.sh restart mychannel 10
+    docker ps -a &&  docker logs -f cli | tee results.log && ./network_setup.sh down
+    grep -q "All GOOD, End-2-End execution completed " results.log
+    if [[ $? -ne 0 ]]; then
+        echo "=============E2E TEST FAILED========="
+        exit 1
+    else
+        echo "=============E2E TEST PASSED=========="
+    fi
 fi
