79e7bca420f795e6c8950b45894597804a59a351
Sambhav Nidamarty <sambhavdutt@gmail.com>
2018-07-25 14:47:50 -0400

    FAB-11265 Update packer with 0.4.10 baseimage.
    The latest thirdparty image 0.4.10 has been pushed
    to Dockerhub. Updating the baseimage version to
    0.4.10 will reduce build time for fabric, fabric-ca
    jobs.
    
    Change-Id: I94b6eb6a30ed765edaac5081a56350c7ae7109da
    Signed-off-by: Sambhav Nidamarty <sambhavdutt@gmail.com>

--- packer/provision/docker.sh
+++ packer/provision/docker.sh
@@ -109,8 +109,7 @@ deb_docker_pull_baseimage() {
     export IMAGES_LIST=(couchdb kafka zookeeper baseimage baseos)
         echo "---> Pulling Thirdparty Images"
         for image in ${IMAGES_LIST[*]}; do
-            docker pull hyperledger/fabric-$image:amd64-0.4.8
-            docker pull hyperledger/fabric-$image:x86_64-0.4.7
+            docker pull hyperledger/fabric-$image:amd64-0.4.10
         done
     echo "---> Pulling Indy images"
     docker pull hyperledger/indy-core-baseci:0.0.1
