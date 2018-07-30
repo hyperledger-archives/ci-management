1084e0957e44e2c657f2cbbc722bf25b671a6a2b
Sambhav Nidamarty <sambhavdutt@gmail.com>
2018-07-30 14:51:47 -0400

    Disable fabric-ca-client
    temporarily disable fabric-ca-client
    
    Change-Id: Ic611a0442411e9515bfad09bb56a1f0ad849e477
    Signed-off-by: Sambhav Nidamarty <sambhavdutt@gmail.com>

--- jjb/fabric-sdk-node/shell/include-raw-npm-release-publish.sh
+++ jjb/fabric-sdk-node/shell/include-raw-npm-release-publish.sh
@@ -32,9 +32,9 @@ versions() {
 cd $WORKSPACE/gopath/src/github.com/hyperledger/fabric-sdk-node
 npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN
 
-cd fabric-ca-client
-versions
-npmPublish fabric-ca-client
+#cd fabric-ca-client
+#versions
+#npmPublish fabric-ca-client
 
 cd ../fabric-client
 versions
