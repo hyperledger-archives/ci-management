e977e4f22b6daa280a297e0e2876e47bcdce04ee
Jessica Wagantall <jwagantall@linuxfoundation.org>
2018-08-24 14:03:54 -0700

    Remove apt and npm in init-scripts
    Do not run apt-get or npm commands in init scripts.
    These should be moved to packer.
    
    Change-Id: I743d263dc715197088094e83d254fdedeb8d9658
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>

--- jenkins-scripts/x86.sh
+++ jenkins-scripts/x86.sh
@@ -14,12 +14,6 @@
 
 # install npm and fabric node sdk dependencies
 curl -sL https://deb.nodesource.com/setup_4.x | sudo bash -
-apt-get install -y nodejs
-apt-get install -y maven
-
-npm install -g typescript
-npm install -g typings
-npm install -g typedoc
 
 cd /usr/local || exit
 wget https://github.com/google/protobuf/releases/download/v3.0.0/protoc-3.0.0-linux-x86_64.zip
