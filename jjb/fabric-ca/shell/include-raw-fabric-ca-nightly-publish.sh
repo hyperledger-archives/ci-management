9c6330c8aa48e60dad69d3bdba0036adccdfb0b3
Sambhav Nidamarty <sambhavdutt@gmail.com>
2018-09-21 14:15:07 -0400

    Fix the script for ca nightly jobs.
    Fix the errors in script for fabric-ca nightly jobs
    
    Change-Id: I911cc1f49509861240e66a7d9b2b429931abd3ab
    Signed-off-by: Sambhav Nidamarty <sambhavdutt@gmail.com>

--- jjb/fabric-ca/shell/include-raw-fabric-ca-nightly-publish.sh
+++ jjb/fabric-ca/shell/include-raw-fabric-ca-nightly-publish.sh
@@ -13,9 +13,13 @@
 # This script publishes the fabric-ca binaries to Nexus2 if
 # the nightly build is successful.
 
-cd $WORKSPACE/gopath/src/github.com/hyperledger/fabric-ca || exit 1
+CA_WD=$WORKSPACE/gopath/src/github.com/hyperledger/fabric-ca
+cd $CA_WD || exit 1
 git checkout $GERRIT_BRANCH
 echo "--------> $GERRIT_BRANCH"
+ORG_NAME=hyperledger/fabric
+NEXUS_REPO=nexus3.hyperledger.org:10003
+
 ARCH=$(go env GOARCH)
 CA_COMMIT=$(git log -1 --pretty=format:"%h")
 echo "---------> FABRIC_CA_COMMIT : $CA_COMMIT"
@@ -26,6 +30,36 @@ echo "-----------> PROJECT_VERSION:" $PROJECT_VERSION
 STABLE_TAG=$ARCH-$PROJECT_VERSION
 echo "-----------> STABLE_TAG:" $STABLE_TAG
 
+# Clone fabric repo
+######################
+rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric
+FABRIC_WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric"
+REPO_NAME=fabric
+
+git clone git://cloud.hyperledger.org/mirror/$REPO_NAME $FABRIC_WD
+cd $FABRIC_WD || exit
+git checkout $GERRIT_BRANCH
+FABRIC_COMMIT=$(git log -1 --pretty=format:"%h")
+echo "----------> FABRIC_COMMIT : $FABRIC_COMMIT"
+echo "FABRIC_COMMIT ----------> $FABRIC_COMMIT" >> commit.log
+mv commit.log ${WORKSPACE}/gopath/src/github.com/hyperledger/
+
+
+build_Fabric() {
+
+# Build fabric images with $PUSH_VERSION tag
+     for IMAGES in docker release-clean $1; do
+         make $IMAGES PROJECT_VERSION=$PUSH_VERSION
+         if [ $? != 0 ]; then
+            echo "----------> make $IMAGES failed"
+            exit 1
+         fi
+     done
+echo
+echo "----------> List all fabric docker images"
+docker images | grep hyperledger || true
+}
+
 build_Fabric_Ca() {
        #### Build fabric-ca docker images
        for IMAGES in docker $2 release-clean $1; do
@@ -34,60 +68,41 @@ build_Fabric_Ca() {
                echo "-------> make $IMAGES failed"
                exit 1
            fi
-               echo
-               echo "-------> List fabric-ca Binary"
        done
-}
-docker images | grep hyperledger/fabric-ca
-
-fabric_Ca_DockerTag() {
-    for IMAGES in ca ca-peer ca-orderer ca-tools $1; do
-         echo "----------> $IMAGES"
-         echo
-         docker tag $ORG_NAME-$IMAGES $NEXUS_URL/$ORG_NAME-$IMAGES:$STABLE_TAG
-         docker tag $ORG_NAME-$IMAGES $NEXUS_URL/$ORG_NAME-$IMAGES:$STABLE_TAG-$CA_COMMIT
-    done
-         echo "----------> $NEXUS_URL/$ORG_NAME-$IMAGES:$STABLE_TAG"
+echo
+echo "----------> List all fabric-ca docker images"
+docker images | grep hyperledger/fabric-ca || true
 }
 
 dockerFabricCaPush() {
     for IMAGES in ca ca-peer ca-orderer ca-tools $1; do
-         echo "-----------> $IMAGES"
-         docker push $NEXUS_URL/$ORG_NAME-$IMAGES:$STABLE_TAG
-         docker push $NEXUS_URL/$ORG_NAME-$IMAGES:$STABLE_TAG-$CA_COMMIT
+         echo "----------> $IMAGES"
          echo
+         docker tag $ORG_NAME-$IMAGES $NEXUS_REPO/$ORG_NAME-$IMAGES:$STABLE_TAG
+         docker tag $ORG_NAME-$IMAGES $NEXUS_REPO/$ORG_NAME-$IMAGES:$STABLE_TAG-$CA_COMMIT
+         docker push $NEXUS_REPO/$ORG_NAME-$IMAGES:$STABLE_TAG
+         docker push $NEXUS_REPO/$ORG_NAME-$IMAGES:$STABLE_TAG-$CA_COMMIT
     done
-         echo "-----------> $NEXUS_URL/$ORG_NAME-$IMAGES:$STABLE_TAG"
+         echo "-----------> $NEXUS_REPO/$ORG_NAME-$IMAGES:$STABLE_TAG"
 }
 
-# Tag Fabric Ca Docker Images
-if [ $ARCH = s390x ]; then
-    fabric_Ca_DockerTag
-else
-    fabric_Ca_DockerTag ca-fvt
-fi
-
-# Push Fabric Ca Docker Images to Nexus3
-if [ $ARCH = s390x ]; then
-    dockerFabricCaPush
-else
-    dockerFabricCaPush ca-fvt
-fi
-# Listout all docker images Before and After Push to NEXUS
-if ! docker images | grep "nexus" ; then
-    echo "------> No nexus images"
-else
-    docker images | grep "nexus*"
-fi
-# Execute release-all target on x arch
 if [ "$ARCH" = "s390x" ]; then
        echo "---------> ARCH:" $ARCH
+       cd $FABRIC_WD
+       build_Fabric dist
+       cd $CA_WD
        build_Fabric_Ca release
+       dockerFabricCaPush
 else
        echo "---------> ARCH:" $ARCH
-       build_Fabric_Ca dist-all
+       cd $FABRIC_WD
+       build_Fabric dist-all
+       cd $CA_WD
+       build_Fabric_Ca dist-all docker-fvt
+       dockerFabricCaPush docker-fvt
 fi
 
+set +e
 # Publish fabric-ca binaries
 curl -L https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric-ca/hyperledger-fabric-ca-$PROJECT_VERSION > output.xml
 # shellcheck disable=SC2034
@@ -95,6 +110,7 @@ CA_RELEASE_COMMIT=$(cat output.xml | grep $CA_COMMIT)
 if [ $? != 1 ]; then
     echo "--------> INFO: $CA_COMMIT is already available... SKIP BUILD"
 else
+set -e
    if [ $ARCH = "amd64" ]; then
        # Push fabric-binaries to nexus2
           for binary in linux-amd64 windows-amd64 darwin-amd64 linux-s390x; do
