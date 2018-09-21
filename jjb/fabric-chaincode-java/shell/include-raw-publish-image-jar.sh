7db6c7dbb5a1f13f0cd9398570edb4541ccc2360
vijaypunugubati <vijaypunugubati@gmail.com>
2018-09-21 10:45:39 -0400

    FAB-12061 Publish Javaenv images
    Update fabric-chaincode-java merge job to publish javaenv
    image to hyperledger dockerhub account after verify the version
    in build.gradle file.
    
    Change-Id: I4d3dd971af06fa752808e0df586c7ce9a082f02a
    Signed-off-by: vijaypunugubati <vijaypunugubati@gmail.com>

--- jjb/fabric-chaincode-java/shell/include-raw-publish-image-jar.sh
+++ jjb/fabric-chaincode-java/shell/include-raw-publish-image-jar.sh
@@ -25,27 +25,25 @@ PROJECT_VERSION=$(cat build.gradle | grep "version =" | awk '{print $3}' | tr -d
 # Build chaincode-javaenv docker image
 ./gradlew buildImage
 
-# tag hyperledger/fabric-javaenv
-	docker tag $ORG_NAME-javaenv $NEXUS_URL/$ORG_NAME-javaenv:$STABLE_TAG
-	docker tag $ORG_NAME-javaenv $NEXUS_URL/$ORG_NAME-javaenv:$STABLE_TAG-$COMMIT_TAG
-
-	# Push javenv docker image
-	docker push $NEXUS_URL/$ORG_NAME-javaenv:$STABLE_TAG
-	docker push $NEXUS_URL/$ORG_NAME-javaenv:$STABLE_TAG-$COMMIT_TAG
-
-#####################
-# PUBLISH JAR FILES
-#####################
+echo "========> gradlew build"
 
 # gladlew build from fabric-chaincode-java repo
 ./gradlew build
-
-# Search for Snapshot to decide the release
-# Publish jars to snapshot repo if PROJECT_VERSION is SNAPSHOT, otherwise to release repo
 # shellcheck disable=SC2046
 if [ `echo $PROJECT_VERSION | grep -c "SNAPSHOT" ` -gt 0 ]; then
+        # if snapshot
+        # tag hyperledger/fabric-javaenv
+	docker tag $ORG_NAME-javaenv $NEXUS_URL/$ORG_NAME-javaenv:$STABLE_TAG
+	docker tag $ORG_NAME-javaenv $NEXUS_URL/$ORG_NAME-javaenv:$STABLE_TAG-$COMMIT_TAG
 
-     for binary in chaincode-shim chaincode-protos; do
+	# Push javenv docker image to nexus3
+        echo "------> PUSHING"
+        docker images
+	docker push $NEXUS_URL/$ORG_NAME-javaenv:$STABLE_TAG
+	docker push $NEXUS_URL/$ORG_NAME-javaenv:$STABLE_TAG-$COMMIT_TAG
+
+# Publish snapshot to Nexus snapshot URL
+    for binary in chaincode-shim chaincode-protos; do
        echo "Pushing fabric-$binary.$PROJECT_VERSION.tar.gz to maven snapshots..."
        tar -czf fabric-$binary.$PROJECT_VERSION.tar.gz *
        mvn org.apache.maven.plugins:maven-deploy-plugin:deploy-file \
@@ -60,10 +58,16 @@ if [ `echo $PROJECT_VERSION | grep -c "SNAPSHOT" ` -gt 0 ]; then
         -DuniqueVersion=false \
         -Dpackaging=tar.gz \
         -gs $GLOBAL_SETTINGS_FILE -s $SETTINGS_FILE
-     done
-     echo "========> DONE <======="
-  else
-     for binary in chaincode-shim chaincode-protos; do
+    done
+       echo "========> DONE <======="
+
+else
+        # if release
+        # Publush fabric-chaincode-javaenv image to Hyperledger DockerHub
+	docker tag $ORG_NAME-javaenv $ORG_NAME-javaenv:amd64-$PROJECT_VERSION
+        docker push $ORG_NAME-javaenv:amd64-$PROJECT_VERSION
+        # Publish chaincode-javaenv binaries to nexus release URL
+    for binary in chaincode-shim chaincode-protos; do
        echo "Pushing fabric-$binary.$PROJECT_VERSION.tar.gz to maven releases.."
        tar -czf fabric-$binary.$PROJECT_VERSION.tar.gz *
        mvn org.apache.maven.plugins:maven-deploy-plugin:deploy-file \
