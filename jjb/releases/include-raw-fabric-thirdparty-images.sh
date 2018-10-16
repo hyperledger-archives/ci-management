4c558b5c97b24da4b24630981589b92eb087192f
vijaypunugubati <vijaypunugubati@gmail.com>
2018-10-15 22:14:53 -0400

    FABCI-164 Update baseimage script
    Delete redundant jobs (nexus, thirdparty) jobs
    from baseimage CI configuration. Add these jobs
    configuration in release jobs to publish thirdparty,
    baseimages to nexus and dockerhub account.
    
    Change-Id: I7a44a52f21ff9d963905a53876318a1f37caa2a0
    Signed-off-by: vijaypunugubati <vijaypunugubati@gmail.com>

--- jjb/releases/include-raw-fabric-thirdparty-images.sh
+++ jjb/releases/include-raw-fabric-thirdparty-images.sh
@@ -14,26 +14,52 @@ set -o pipefail
 MARCH=$(go env GOARCH)
 DEPENDENT_TAG=$MARCH-$(make -f Makefile -f <(printf 'p:\n\t@echo $(VERSION)\n') p)
 echo "DEPENDENT_TAG Images TAG ID is: " $DEPENDENT_TAG
-echo
 ORG_NAME="hyperledger/fabric"
+NEXUS_REPO_URL="nexus3.hyperledger.org:10002"
+ARCH=$(dpkg --print-architecture)
+echo "----------> ARCH:" $ARCH
 
-docker login --username=$DOCKER_HUB_USERNAME --password=$DOCKER_HUB_PASSWORD
-# Push docker images to dockerhub
-
-dockerFabricPush() {
+dockerBaseImages() {
 
   # shellcheck disable=SC2043
-  for IMAGES in couchdb kafka zookeeper; do
+  for IMAGES in baseimage baseos basejvm; do
     echo "==> $IMAGES"
-    docker push $ORG_NAME-$IMAGES:$DEPENDENT_TAG
-    echo
-    echo "==> $ORG_NAME-$IMAGES:$DEPENDENT_TAG"
-    echo
+    docker tag $ORG_NAME-$IMAGES:$DEPENDENT_TAG $NEXUS_REPO_URL/$ORG_NAME-$IMAGES:$DEPENDENT_TAG
+    echo "==> $NEXUS_REPO_URL/$ORG_NAME-$IMAGES:$DEPENDENT_TAG"
+    docker push $NEXUS_REPO_URL/$ORG_NAME-$IMAGES:$DEPENDENT_TAG
   done
 }
 
-# Push Fabric Docker Images to hyperledger dockerhub account
-dockerFabricPush
+#docker login --username=$DOCKER_HUB_USERNAME --password=$DOCKER_HUB_PASSWORD
+# Push docker images to dockerhub
+
+dockerThirdpartyImage() {
+
+  # shellcheck disable=SC2043
+  for IMAGES in couchdb kafka zookeeper; do
+    echo "-------> Publish thirdparty Docker images to DockerHub"
+    docker push $ORG_NAME-$IMAGES:$DEPENDENT_TAG
+    echo
+    echo "-------> $ORG_NAME-$IMAGES:$DEPENDENT_TAG"
+    echo
+    docker tag $ORG_NAME-$IMAGES:$DEPENDENT_TAG $NEXUS_REPO_URL/$ORG_NAME-$IMAGES:$DEPENDENT_TAG
+    echo "-------> $NEXUS_REPO_URL/$ORG_NAME-$IMAGES:$DEPENDENT_TAG"
+    echo "-------> Publish thirdparty Docker images to Nexus3"
+    docker push $NEXUS_REPO_URL/$ORG_NAME-$IMAGES:$DEPENDENT_TAG
+  done
+}
+
+echo "--------> Publish baseimage to DockerHub"
+make install
+
+echo "--------> Publish baseimage to Nexus3
+dockerBaseImages
+
+echo "--------> Build Thirdparty Docker images"
+make dependent-images
+
+echo "--------> Publish Thirdparty Docker images to DockerHub and Nexus3
+dockerThirdpartyImage
 
 # Listout all docker images
 docker images | grep "hyperledger*"
