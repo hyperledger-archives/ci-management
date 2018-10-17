6a977d9b46d4c2a5ab444e88ffa957ffbdfa11a5
vijaypunugubati <vijaypunugubati@gmail.com>
2018-10-17 09:17:27 -0400

    FABCI-169 Publish images to nexus in release jobs
    Update CI script to publish fabric images to nexus
    in fabric-release-jobs.
    
    Change-Id: I19527b03eb614382b58ffb9a9bdb519b49ed8e5a
    Signed-off-by: vijaypunugubati <vijaypunugubati@gmail.com>

---
+++ jjb/fabric/shell/include-raw-fabric-publish-nexus-images.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -eu
+#
+# SPDX-License-Identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018 IBM Corporation, The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License 2.0
+# which accompanies this distribution, and is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+set -o pipefail
+
+ORG_NAME="hyperledger/fabric"
+NEXUS_REPO_URL=nexus3.hyperledger.org:10002
+
+# tag fabric images to nexusrepo
+
+dockerTag() {
+  for IMAGES in ${IMAGES_LIST[*]}; do
+    docker tag $ORG_NAME-$IMAGES $NEXUS_REPO_URL/$ORG_NAME-$IMAGES:$ARCH-$1
+    echo "==> $NEXUS_REPO_URL/$ORG_NAME-$IMAGES:$ARCH-$1"
+  done
+}
+# Push fabric images to nexus repository
+
+docker_Fabric_Push() {
+  for IMAGES in ${IMAGES_LIST[*]}; do
+    docker push $NEXUS_REPO_URL/$ORG_NAME-$IMAGES:$ARCH-$1
+    echo "==> $NEXUS_REPO_URL/$ORG_NAME-$IMAGES:$ARCH-$1"
+  done
+}
+
+if [[ "$GERRIT_BRANCH" = "release-1.0" ]]; then
+   IMAGES_LIST=(peer orderer ccenv tools zookeeper kafka couchdb javaenv)
+   # Tag & Push Fabric Docker Images to Nexus Repository
+   echo "==== Tag Images ===="
+   dockerTag $PUSH_VERSION
+   echo "==== Push Images to Nexus ===="
+   docker_Fabric_Push $PUSH_VERSION
+elif [[ "$GERRIT_BRANCH" = "release-1.1" ]]; then
+   IMAGES_LIST=(peer orderer ccenv tools javaenv)
+   # Tag & Push Fabric Docker Images to Nexus Repository
+   echo "==== Tag Images ===="
+   dockerTag $PUSH_VERSION
+   echo "==== Push Images to Nexus ===="
+   docker_Fabric_Push $PUSH_VERSION
+else
+   IMAGES_LIST=(peer orderer ccenv tools)
+   # Tag & Push Fabric Docker Images to Nexus Repository
+   echo "==== Tag Images ===="
+   dockerTag $PUSH_VERSION
+   echo "==== Push Images to Nexus ===="
+   docker_Fabric_Push $PUSH_VERSION
+fi
+
+# Listout all docker images Before and After Push to NEXUS
+docker images | grep "nexus*"
