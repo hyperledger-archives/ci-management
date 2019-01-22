0ba9168b8f6e8cf0563a4ba2c5c68d47d66c9223
rameshthoomu <rameshbabu.thoomu@gmail.com>
2019-01-22 09:22:44 -0500

    FABCI-253 Remove the nvm step & fix broken builds
    This patch removes the nvm installation step from all s390x
    builds. The nvm configuration has been setup in all prod and
    sandbox s390x build nodes. Instead of installing nvm, just source
    the nvmrc.sh file before call nvm.
    
    - Add softhsm code for all s390x builds to fix the unit-test
    code coverage issues on s390x
    
    - Enable Java E2E tests on fabric, fabric-ca e2e jobs. SDK java is now
    working on 2.0.0.
    
    - Update clean environment script to delete dead code
    
    Change-Id: I0cc30576330d1a8648949092abd202bb50ef34d6
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- jjb/common-scripts/include-raw-fabric-clean-environment.sh
+++ jjb/common-scripts/include-raw-fabric-clean-environment.sh
@@ -9,46 +9,48 @@
 # which accompanies this distribution, and is available at
 # https://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
+set -o pipefail
 
 echo "----> include-raw-fabric-clean-environment.sh"
 
-function clearContainers () {
+clearContainers () {
         CONTAINER_IDS=$(docker ps -aq)
         if [ -z "$CONTAINER_IDS" ] || [ "$CONTAINER_IDS" = " " ]; then
-                echo "---- No containers available for deletion ----"
+             echo "---- No containers available for deletion ----"
         else
-                docker rm -f $CONTAINER_IDS || true
-                docker ps -a
+             docker rm -f $CONTAINER_IDS
+             echo -e "\033[32m Docker Container List \033[0m"
+             docker ps -a
         fi
 }
 
-function removeUnwantedImages() {
-        for i in $(docker images | grep none | awk '{print $3}'); do
-		docker rmi ${i} || true
-        done
-
-	for i in $(docker images | grep -vE ".*baseimage.*(0.4.13|0.4.14)" | grep -vE ".*baseos.*(0.4.13|0.4.14)" | grep -vE ".*couchdb.*(0.4.13|0.4.14)" | grep -vE ".*zoo.*(0.4.13|0.4.14)" | grep -vE ".*kafka.*(0.4.13|0.4.14)" | grep -v "REPOSITORY" | awk '{print $1":" $2}'); do
-	        docker rmi ${i} || true
-        done
-		docker images
+removeUnwantedImages() {
+        # Delete <none> images
+        docker images | grep none | awk '{ print $3; }' | xargs docker rmi || true
+        # Get the latest baseimage version from Makefile of fabric master branch
+        curl -L https://raw.githubusercontent.com/hyperledger/fabric/master/Makefile > Makefile
+        # Fetch baseimage release version
+        BASE_IMAGE=$(cat Makefile | grep "BASEIMAGE_RELEASE =" | awk '{print $3}')
+        # Deleete Makefile
+        rm -rf Makefile
+        # Delete all docker images except the latest one fetched from fabric master Makefile
+        IMAGE_IDS=$(docker images | grep -v "$BASE_IMAGE" | awk 'NR>1 {print $3}')
+        if [[ -z ${IMAGE_IDS// } ]]; then
+             echo "---- No Images available for deletion ----"
+        else
+             # Delete all list docker images
+             docker rmi -f $IMAGE_IDS
+             echo -e "\033[32m Docker Images List \033[0m"
+             docker images
+        fi
 }
 
-# Delete nvm prefix & then delete nvm
-rm -rf $HOME/.nvm/ $HOME/.node-gyp/ $HOME/.npm/ $HOME/.npmrc  || true
+# Remove /tmp/fabric-shim as root permissions set on it. see FABCI-61
+docker run -v /tmp:/tmp library/alpine rm -rf /tmp/fabric-shim
 
-mkdir $HOME/.nvm || true
-
-# Remove /tmp/fabric-shim
-docker run -v /tmp:/tmp library/alpine rm -rf /tmp/fabric-shim || true
-
-# remove tmp/hfc and hfc-key-store data
-rm -rf /home/jenkins/.nvm /home/jenkins/npm /tmp/fabric-shim /tmp/hfc* /tmp/npm* /home/jenkins/kvsTemp /home/jenkins/.hfc-key-store
-
+# remove leftovers from the previous builds
+rm -rf /home/jenkins/npm /tmp/fabric-shim /tmp/hfc* /tmp/npm* /home/jenkins/kvsTemp /home/jenkins/.hfc-key-store
 rm -rf /var/hyperledger/*
-
-rm -rf gopath/src/github.com/hyperledger/fabric-ca/vendor/github.com/cloudflare/cfssl/vendor/github.com/cloudflare/cfssl_trust/ca-bundle || true
-# yamllint disable-line rule:line-length
-rm -rf gopath/src/github.com/hyperledger/fabric-ca/vendor/github.com/cloudflare/cfssl/vendor/github.com/cloudflare/cfssl_trust/intermediate_ca || true
 
 clearContainers
 removeUnwantedImages
