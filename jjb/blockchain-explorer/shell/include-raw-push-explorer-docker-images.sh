3d577e36216bc00303a7db7b053bb1388e08fdd7
Sambhav Nidamarty <sambhavdutt@gmail.com>
2019-03-08 17:21:15 -0500

    FABCI-310 Add version tag for release images
    Tag blockchain explorer images with the version in
    the blockchain-explorer release process.
    
    Change-Id: If7f51f4f2dfd8085a81d7d0db7e1922391a76dc1
    Signed-off-by: Sambhav Nidamarty <sambhavdutt@gmail.com>

--- jjb/blockchain-explorer/shell/include-raw-push-explorer-docker-images.sh
+++ jjb/blockchain-explorer/shell/include-raw-push-explorer-docker-images.sh
@@ -3,17 +3,22 @@
 ORG_NAME="hyperledger"
 IMAGES_LIST=(explorer explorer-db)
 export IMAGES_LIST
+TAG=$(git describe --abbrev=0 --tags | cut -d 'v' -f 2)
 docker login --username=$DOCKER_HUB_USERNAME --password=$DOCKER_HUB_PASSWORD
 
 # Clone blockchain-explorer git repository
 clone_Blockchain_Explorer() {
- rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/blockchain-explorer
- WD="${WORKSPACE}/gopath/src/github.com/hyperledger/blockchain-explorer"
- REPO_NAME=blockchain-explorer
- git clone --single-branch -b $GERRIT_BRANCH git://cloud.hyperledger.org/mirror/$REPO_NAME $WD
- cd $WD && git checkout $GERRIT_BRANCH && git checkout $RELEASE_COMMIT
- echo "-------> INFO: RELEASE_COMMIT" $RELEASE_COMMIT
+    rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/blockchain-explorer
+    WD="${WORKSPACE}/gopath/src/github.com/hyperledger/blockchain-explorer"
+    REPO_NAME=blockchain-explorer
+    git clone --single-branch -b $GERRIT_BRANCH git://cloud.hyperledger.org/mirror/$REPO_NAME $WD
+    cd $WD
+    git checkout $GERRIT_BRANCH
+    RELEASE_COMMIT=$(git log -1 --pretty=format:"%h")
+    echo "-------> INFO: RELEASE_COMMIT" $RELEASE_COMMIT
+    echo "-------> INFO: RELEASE TAG: $TAG"
 }
+
 
 # Build blockchain-explorer images
 docker_Build_Images() {
@@ -25,21 +30,21 @@ docker_Build_Images() {
 docker images | grep hyperledger
 
 docker_Explorer_Push() {
-   # Clone blockchain-explorer
-   clone_Blockchain_Explorer
-   # Call to build blockchain-explorer images
-   docker_Build_Images
- for IMAGES in ${IMAGES_LIST[*]}; do
-   # Tag with latest & ($PUSH_VERSION)
-   docker tag $ORG_NAME/$IMAGES $ORG_NAME/$IMAGES:$1
-   docker tag $ORG_NAME/$IMAGES $ORG_NAME/$IMAGES
-   echo "------> Push blockchain-explorer Images to hyperledger dockerhub Repository"
-   docker push $ORG_NAME/$IMAGES:$1
-   docker push $ORG_NAME/$IMAGES
- done
+    # Clone blockchain-explorer
+    clone_Blockchain_Explorer
+    # Call to build blockchain-explorer images
+    docker_Build_Images
+    for IMAGES in ${IMAGES_LIST[*]}; do
+      # Tag with latest & ($PUSH_VERSION)
+      docker tag $ORG_NAME/$IMAGES $ORG_NAME/$IMAGES:$1
+      docker tag $ORG_NAME/$IMAGES $ORG_NAME/$IMAGES
+      echo "------> Push blockchain-explorer Images to hyperledger dockerhub Repository"
+      docker push $ORG_NAME/$IMAGES:$1
+      docker push $ORG_NAME/$IMAGES
+    done
 }
 
-docker_Explorer_Push $PUSH_VERSION
+docker_Explorer_Push $TAG
 
 # list docker images
 docker images | grep hyperledger
