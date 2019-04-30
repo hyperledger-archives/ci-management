f50b991bd5ddfd8b01a8dd7b81cca2df8e9601a3
Tim Johnson <tijohnson@linuxfoundation.org>
2019-04-30 07:58:29 -0700

    Cleanup verify-commit.sh
    Add switch based on project to locate the top of the repo
    
    FABCI-339
    Change-Id: I505af05966d32f04ad0071b26a0dac81c42b98e4
    Signed-off-by: Tim Johnson <tijohnson@linuxfoundation.org>

--- scripts/verify-commit.sh
+++ scripts/verify-commit.sh
@@ -16,11 +16,33 @@
 # Verify if the commit message contains JIRA URLs.
 # its-jira pluggin attempts to process jira links and breaks.
 
-set +ue # Temporarily ignore any errors
-
 set -o pipefail
 echo "----> verify-commit.sh"
 
+echo "########  DEBUG  ########"
+echo "WORKSPACE = $WORKSPACE"
+echo "BASE_DIR  = $BASE_DIR"
+echo "PROJECT   = $PROJECT"
+echo "########  DEBUG  ########"
+
+# Until project names are added to the switch statement below
+exit 0
+
+case $PROJECT in
+    project1|project2|project3) ;;&
+    fabric-sdk-py)
+        cd $WORKSPACE/$BASE_DIR ;;
+    project100|project101|project102) ;;&
+    project200|project201|project202) ;;&
+    ci-management*)
+        cd $WORKSPACE ;;
+    *) echo "FATAL: Unknown Project" ; exit -1;;
+esac
+
+# Exit with error if we are not inside a repo
+git rev-parse --is-inside-git-dir > /dev/null
+
+# Check for JIRA URL's in the Commit Message
 if git rev-list --format=%B --max-count=1 HEAD | grep -io 'http[s]*://jira\..*' > /dev/null ; then
     echo 'Error: Remove JIRA URLs from commit message'
     echo 'Add jira references as: Issue: <JIRAKEY>-<ISSUE#>, instead of URLs'
@@ -28,13 +50,14 @@ if git rev-list --format=%B --max-count=1 HEAD | grep -io 'http[s]*://jira\..*' 
 fi
 
 # Check for trailing white-space (tab or spaces) in any files that were changed
+echo "Checking for trailing white-space..."
 commit_files=$(git diff-tree --name-only -r HEAD~1..HEAD)
-
 found_trailing=false
 for filename in $commit_files; do
     if [[ $(file -b $filename) == "ASCII text"* ]]; then
         if egrep -q "\s$" $filename; then
             found_trailing=true
+            ascii_file_list+="$filename "
             echo "Error: Trailing white spaces found in file: $filename"
         fi
     fi
@@ -42,6 +65,6 @@ done
 
 if $found_trailing; then
     echo "####  filename:line-num:line  ####"
-    egrep -n  "\s$" $commit_files
+    egrep -Hn "\s$" $ascii_file_list
     exit 1
 fi
