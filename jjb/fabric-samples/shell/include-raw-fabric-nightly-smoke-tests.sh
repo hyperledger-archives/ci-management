c98ac60e88d45994f96ee2a544b859bce647dfed
Sambhav Nidamarty <sambhavdutt@gmail.com>
2019-05-21 20:42:15 -0400

    Modify the BYFN tests in publish jobs
    Dont run the couchdb tests for s390x on the
    master branch as couchdb 2.3 is not available
    in the s390x/z platform.
    
    Issue: FABCI-368
    
    Change-Id: I9333c1464bc02cbcb70f6047e8b11e6f1d428ec0
    Signed-off-by: Sambhav Nidamarty <sambhavdutt@gmail.com>

--- jjb/fabric-samples/shell/include-raw-fabric-nightly-smoke-tests.sh
+++ jjb/fabric-samples/shell/include-raw-fabric-nightly-smoke-tests.sh
@@ -11,31 +11,32 @@
 ##############################################################################
 
 # docker container list
-CONTAINER_LIST=(peer0.org1 peer1.org1 peer0.org2 peer1.org2 peer0.org3 peer1.org3 orderer)
-COUCHDB_CONTAINER_LIST=(couchdb0 couchdb1 couchdb2 couchdb3 couchdb4 couchdb5)
+container_list=(peer0.org1 peer1.org1 peer0.org2 peer1.org2 peer0.org3 peer1.org3 orderer)
+couchdb_container_list=(couchdb0 couchdb1 couchdb2 couchdb3 couchdb4 couchdb5)
 
 # Clone fabric-samples.
 ######################
 rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-samples
-WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-samples"
-REPO_NAME=fabric-samples
+wd="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-samples"
+repo_name=fabric-samples
 
 echo "######## Cloning fabric-samples ########"
-git clone git://cloud.hyperledger.org/mirror/$REPO_NAME $WD
-cd $WD || exit
+git clone git://cloud.hyperledger.org/mirror/$repo_name $wd
+cd $wd || exit
 git checkout $GERRIT_BRANCH
 echo "-------> GERRIT_BRANCH: $GERRIT_BRANCH"
-FABRIC_SAMPLES_COMMIT=$(git log -1 --pretty=format:"%h")
-echo "FABRIC_SAMPLES_COMMIT ========> $FABRIC_SAMPLES_COMMIT" >> ${WORKSPACE}/gopath/src/github.com/hyperledger/commit.log
+fabric_samples_commit=$(git log -1 --pretty=format:"%h")
+echo "FABRIC_SAMPLES_COMMIT ========> $fabric_samples_commit" >> \
+  ${WORKSPACE}/gopath/src/github.com/hyperledger/commit.log
 
 # copy /bin directory to fabric-samples
 
-if [ "$ARCH" == "s390x" ]; then
-   cp -r $WORKSPACE/gopath/src/github.com/hyperledger/fabric/release/linux-s390x/bin/ .
-elif [ "$ARCH" == "ppc64le" ]; then
-   cp -r $WORKSPACE/gopath/src/github.com/hyperledger/fabric/release/linux-ppc64le/bin/ .
+if [[ "$ARCH" == "s390x" ]]; then
+    cp -r $WORKSPACE/gopath/src/github.com/hyperledger/fabric/release/linux-s390x/bin/ .
+elif [[ "$ARCH" == "ppc64le" ]]; then
+    cp -r $WORKSPACE/gopath/src/github.com/hyperledger/fabric/release/linux-ppc64le/bin/ .
 else
-   cp -r $WORKSPACE/gopath/src/github.com/hyperledger/fabric/release/linux-amd64/bin/ .
+    cp -r $WORKSPACE/gopath/src/github.com/hyperledger/fabric/release/linux-amd64/bin/ .
 fi
 
 cd first-network || exit
@@ -44,7 +45,7 @@ cd first-network || exit
 mkdir -p $WORKSPACE/Docker_Container_Logs
 
 #Set INFO to DEBUG
-sed -it 's/INFO/DEBUG/' base/peer-base.yaml
+sed -it 's/INFO/DEBUG/g' base/peer-base.yaml
 
 export PATH=gopath/src/github.com/hyperledger/fabric-samples/bin:$PATH
 
@@ -59,67 +60,92 @@ artifacts() {
 # Capture docker logs of each container
 logs() {
 
-for CONTAINER in ${CONTAINER_LIST[*]}; do
-    docker logs $CONTAINER.example.com >& $WORKSPACE/Docker_Container_Logs/$CONTAINER-$1.log
-    echo
-done
-
-if [ ! -z $2 ]; then
-
-    for CONTAINER in ${COUCHDB_CONTAINER_LIST[*]}; do
-        docker logs $CONTAINER >& $WORKSPACE/Docker_Container_Logs/$CONTAINER-$1.log
+    echo -e "\n ===== Collecting logs for test =======\n"
+    for container in ${container_list[*]}; do
+        docker logs $container.example.com >& $WORKSPACE/Docker_Container_Logs/$container-$1.log
         echo
     done
-fi
+
+    if [[ -n $2 ]]; then
+        for container in ${couchdb_container_list[*]}; do
+            docker logs $container >& $WORKSPACE/Docker_Container_Logs/$container-$1.log
+            echo
+        done
+    fi
 }
 
 copy_logs() {
 
-# Call logs function
-logs $2 $3
+    # Call logs function
+    if [[ "$#" -gt 0 ]]; then
+        for arg in "$@"; do
+            logs $arg
+        done
+    fi
 
-if [ $1 != 0 ]; then
-    artifacts
-    exit 1
-fi
+    if [[ $1 != 0 ]]; then
+        artifacts
+        exit 1
+    fi
 }
 
-# Execute below tests
+# Delete existing docker containers
+echo "------> Deleting Containers...."
+# shellcheck disable=SC2046
+docker rm -f $(docker ps -aq) || true
+echo "------> List Docker Containers"
+docker ps -aq
+
+# Execute BYFN tests
 echo "------> BRANCH: " $GERRIT_BRANCH
-echo "############## BYFN,EYFN DEFAULT CHANNEL TEST ###################"
-echo "#################################################################"
+defaultchannel() {
+    echo -e "############## \033[1mD E F A U L T-C H A N N E L\033[0m ###########"
+    echo "#########################################################"
+    set -x
+    echo y | ./byfn.sh -m down
+    echo y | ./byfn.sh -m up -t 120 -d 20; copy_logs $? default-channel
+    echo y | ./eyfn.sh -m up -t 120 -d 20; copy_logs $? default-channel
+    echo y | ./eyfn.sh -m down
+    set +x
+    echo
+}
+customchannel() {
+    echo -e "############## \033[1mC U S T O M-C H A N N E L\033[0m ################"
+    echo "#########################################################"
+    set -x
+    echo y | ./byfn.sh -m up -c custom-channel -t 120 -d 20; copy_logs $? custom-channel
+    echo y | ./eyfn.sh -m up -c custom-channel -t 120 -d 20; copy_logs $? custom-channel
+    echo y | ./eyfn.sh -m down
+    set +x
+    echo
+}
+couchdb() {
+    echo -e "############### \033[1mC O U C H D B-T E S T\033[0m ###################################"
+    echo "#########################################################################"
+    set -x
+    echo y | ./byfn.sh -m up -c custom-channel-couchdb -s couchdb -t 100 -d 20; copy_logs $? custom-channel-couch couchdb
+    echo y | ./eyfn.sh -m up -c custom-channel-couchdb -s couchdb -t 100 -d 20; copy_logs $? custom-channel-couch couchdb
+    echo y | ./eyfn.sh -m down
+    set +x
+    echo
+}
+nodechaincode() {
+    echo -e "############### \033[1mN O D E-C H A I N C O D E\033[0m ################"
+    echo "####################################################################"
+    set -x
+    echo y | ./byfn.sh -m up -l node -t 60; copy_logs $? default-channel-node
+    echo y | ./eyfn.sh -m up -l node -t 60; copy_logs $? default-channel-node
+    echo y | ./eyfn.sh -m down
+    set +x
+}
 
-echo y | ./byfn.sh -m down
-echo y | ./byfn.sh -m up -t 60
-copy_logs $? default-channel
-echo y | ./eyfn.sh -m up -t 60
-copy_logs $? default-channel
-echo y | ./eyfn.sh -m down
-echo
-: '
-echo "############## BYFN,EYFN CUSTOM CHANNEL TEST ############"
-echo "#########################################################"
-
-echo y | ./byfn.sh -m up -c custom-channel -t 60
-copy_logs $? custom-channel
-echo y | ./eyfn.sh -m up -c custom-channel -t 60
-copy_logs $? custom-channel
-echo y | ./eyfn.sh -m down
-echo
-echo "############### BYFN,EYFN CUSTOM CHANNEL WITH COUCHDB TEST ##############"
-echo "#########################################################################"
-'
-echo y | ./byfn.sh -m up -c custom-channel-couchdb -s couchdb -t 60 -d 15
-copy_logs $? custom-channel-couch couchdb
-echo y | ./eyfn.sh -m up -c custom-channel-couchdb -s couchdb -t 60 -d 15
-copy_logs $? custom-channel-couch couchdb
-echo y | ./eyfn.sh -m down
-echo
-echo "############### BYFN,EYFN WITH NODE Chaincode. TEST ################"
-echo "####################################################################"
-
-echo y | ./byfn.sh -m up -l node -t 60
-copy_logs $? default-channel-node
-echo y | ./eyfn.sh -m up -l node -t 60
-copy_logs $? default-channel-node
-echo y | ./eyfn.sh -m down
+if [[ $GERRIT_BRANCH = "master" && $ARCH = "s390x" ]]; then
+    defaultchannel
+    customchannel
+    nodechaincode
+else
+    defaultchannel
+    customchannel
+    couchdb
+    nodechaincode
+fi
