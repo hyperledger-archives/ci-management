94a752fd7f020c306a333f2398a705cb43961d4b
Tim Johnson <tijohnson@linuxfoundation.org>
2019-08-28 16:30:19 -0700

    Update Jenkins Init Script
    Change-Id: Ie40d2d7afff66bf65f6b1dfc3ad7cf4ce861490f
    Signed-off-by: Tim Johnson <tijohnson@linuxfoundation.org>

--- jenkins-init-scripts/init.sh
+++ jenkins-init-scripts/init.sh
@@ -1,18 +1,24 @@
-#! /bin/bash -ue
+#! /bin/bash -u
 
 # This script get called from the Jenkins Init Script that runs on the
 # build agent before the build scripts are called. It runs as 'root'
+# All errors are ignored (+e)
 
-# No explaination, it is required
-chown -R jenkins:jenkins /var/lib/softhsm /etc/softhsm
+# Jenkins needs to own these directories sigh...
+# softhsm may not be installed in all images
+if [[ -d /etc/softhsm ]]; then
+    chown -R jenkins:jenkins /var/lib/softhsm /etc/softhsm
+fi
 
-# Create HY Env file
+# Create HL Env file
 # This file contains a library of bash functions & aliases
-cat <<EOF > ~jenkins/hl-env
-function echo_red()     { echo -ne "\033[91m$*\033[0m" ;}
-function echo_green()   { echo -ne "\033[32m$*\033[0m" ;}
-function echo_yellow()  { echo -ne "\033[93m$*\033[0m" ;}
-function echo_blue()    { echo -ne "\033[96m$*\033[0m" ;}
-function echo_reverse() { echo -ne "\033[7m$*\033[0m"  ;}
+env_file=/home/jenkins/hl-env
+cat <<EOF > $env_file
+function echo_red()     { echo -ne "\\033[91m$*\\033[0m" ;}
+function echo_green()   { echo -ne "\\033[32m$*\\033[0m" ;}
+function echo_yellow()  { echo -ne "\\033[93m$*\\033[0m" ;}
+function echo_blue()    { echo -ne "\\033[96m$*\\033[0m" ;}
+function echo_reverse() { echo -ne "\\033[7m$*\\033[0m"  ;}
 function echo_error()   { echo -ne "FATAL - $*"        ;}
 EOF
+chown jenkins:jenkins $env_file
