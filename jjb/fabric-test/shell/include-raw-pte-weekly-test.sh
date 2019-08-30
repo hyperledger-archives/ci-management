9c933e08eecf2952061ea94fd4a6a52a31975ca6
vijaypunugubati <vijaypunugubati@gmail.com>
2019-08-30 10:41:39 -0400

    FABCI-409 Cleanup pte-weekly-test script
    Removing redundant code from pte-weekly script which
    is getting old binaries from releases folder of nexus
    repo.
    
    Signed-off-by: vijaypunugubati <vijaypunugubati@gmail.com>
    Change-Id: Ida6a062a58359aa1e3913b2367e29b154909c29b

--- jjb/fabric-test/shell/include-raw-pte-weekly-test.sh
+++ jjb/fabric-test/shell/include-raw-pte-weekly-test.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -e
+#!/bin/bash -ue
+set -o pipefail
 #
 # SPDX-License-Identifier: Apache-2.0
 ##############################################################################
@@ -11,42 +12,38 @@
 ##############################################################################
 
 checkout_project() {
-  rm -rf $WORKSPACE/gopath/src/github.com/hyperledger/fabric-test
-  WD="$WORKSPACE/gopath/src/github.com/hyperledger/fabric-test"
-  FABRIC_TEST_REPO_NAME=fabric-test
-  git clone git://cloud.hyperledger.org/mirror/$FABRIC_TEST_REPO_NAME $WD
-  cd $WD
-  git checkout $GERRIT_BRANCH
-}
-
-get_release_commit() {
-  MVN_METADATA=$(echo "https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric-$STABLE_VERSION/maven-metadata.xml")
-  curl -L "$MVN_METADATA" > maven-metadata.xml
-  RELEASE_TAG=$(cat maven-metadata.xml | grep release)
-  RELEASE_COMMIT=$(echo $RELEASE_TAG | awk -F - '{ print $4 }' | cut -d "<" -f1)
-  export RELEASE_COMMIT
-  echo "---------> RELEASE_COMMIT:" $RELEASE_COMMIT
+    cd $WORKSPACE/gopath/src/github.com/hyperledger
+    local repo_name=fabric-test
+    rm -rf $repo_name
+    git clone git://cloud.hyperledger.org/mirror/$repo_name
+    cd $repo_name
+    git checkout $GERRIT_BRANCH
 }
 
 run_12hr_pte_test() {
-  cd $WORKSPACE/gopath/src/github.com/hyperledger/fabric-test
-  # Fetch Go Version from fabric ci.properties file
-  curl -L https://raw.githubusercontent.com/hyperledger/fabric/$GERRIT_BRANCH/ci.properties > ci.properties
-  GO_VER=`cat ci.properties | grep GO_VER | cut -d "=" -f 2`
-  export GO_VER
-  OS_VER=$(dpkg --print-architecture)
-  echo "------> ARCH: $OS_VER"
-  export GOROOT=/opt/go/go$GO_VER.linux.$OS_VER
-  export PATH=$GOROOT/bin:$PATH
-  echo "------> GO_VER" $GO_VER
-  # Execute Make target from fabric-test Makefile
-  make svt-weekly-pte-12hr-test
+    cd $WORKSPACE/gopath/src/github.com/hyperledger/fabric-test
+    # Fetch Go Version from fabric ci.properties file
+    curl -L https://raw.githubusercontent.com/hyperledger/fabric/$GERRIT_BRANCH/ci.properties > ci.properties
+    GO_VER=`cat ci.properties | grep GO_VER | cut -d "=" -f 2`
+    eval "$(grep GO_VER ci.properties)"
+        if [ -z "$GO_VER" ]; then
+            echo "-----> Empty GO_VER"
+            exit 1
+        fi
+    echo "GO_VER="$GO_VER
+    export GO_VER
+    OS_VER=$(dpkg --print-architecture)
+    echo "------> ARCH: $OS_VER"
+    export GOROOT=/opt/go/go$GO_VER.linux.$OS_VER
+    export PATH=$GOROOT/bin:$PATH
+    echo "------> GO_VER" $GO_VER
+    # Execute Make target from fabric-test Makefile
+    make svt-weekly-pte-12hr-test
 }
 
 main() {
-  checkout_project
-  get_release_commit
-  run_12hr_pte_test
+    checkout_project
+    run_12hr_pte_test
 }
 
 main
